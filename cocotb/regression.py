9e2a5e0c6f9ab5cd55f9ada6f4f5e973ad7561b7
Gordon McGregor <gordon.mcgregor@verilab.com>
2013-07-11 11:37:09 +0000

    reset

--- cocotb/regression.py
+++ cocotb/regression.py
@@ -1,4 +1,4 @@
-    ''' Copyright (c) 2013 Potential Ventures Ltd
+''' Copyright (c) 2013 Potential Ventures Ltd
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
