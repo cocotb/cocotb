073744b496d40e3274c9ff09a9443139b6dddcf9
Fabien Marteau <fabien.marteau@armadeus.com>
2015-09-21 12:40:23 +0200

    test burst with address 0

--- tests/test_cases/test_avalon/test_avalon.py
+++ tests/test_cases/test_avalon/test_avalon.py
@@ -41,6 +41,49 @@ from cocotb.triggers import (Timer, Join, RisingEdge, FallingEdge, Edge,
 from cocotb.clock import Clock
 from cocotb.result import ReturnValue, TestFailure, TestError, TestSuccess
 
+class BurstAvlReadTest(object):
+    """ class to test avalon burst """
+
+    def __init__(self, dut):
+        self.dut = dut
+        # Launch clock
+        dut.reset = 1
+        clk_gen = cocotb.fork(Clock(dut.clk, 10).start())
+
+        # Bytes aligned memory
+        self.memdict = {
+                   0x00: 0x00, 0x10: 0x10, 0x20: 0x20, 0x30: 0x30,
+                   0x01: 0x01, 0x11: 0x11, 0x21: 0x21, 0x31: 0x31,
+                   0x02: 0x02, 0x12: 0x12, 0x22: 0x22, 0x32: 0x32,
+                   0x03: 0x03, 0x13: 0x13, 0x23: 0x23, 0x33: 0x33,
+                   0x04: 0x04, 0x14: 0x14, 0x24: 0x24, 0x34: 0x34,
+                   0x05: 0x05, 0x15: 0x15, 0x25: 0x25, 0x35: 0x35,
+                   0x06: 0x06, 0x16: 0x16, 0x26: 0x26, 0x36: 0x36,
+                   0x07: 0x07, 0x17: 0x17, 0x27: 0x27, 0x37: 0x37,
+                   0x08: 0x08, 0x18: 0x18, 0x28: 0x28, 0x38: 0x38,
+                   0x09: 0x09, 0x19: 0x19, 0x29: 0x29, 0x39: 0x39,
+                   0x0a: 0x0a, 0x1a: 0x1a, 0x2a: 0x2a, 0x3a: 0x3a,
+                   0x0b: 0x0b, 0x1b: 0x1b, 0x2b: 0x2b, 0x3b: 0x3b,
+                   0x0c: 0x0c, 0x1c: 0x1c, 0x2c: 0x2c, 0x3c: 0x3c,
+                   0x0d: 0x0d, 0x1d: 0x1d, 0x2d: 0x2d, 0x3d: 0x3d,
+                   0x0e: 0x0e, 0x1e: 0x1e, 0x2e: 0x2e, 0x3e: 0x3e,
+                   0x0f: 0x0f, 0x1f: 0x1f, 0x2f: 0x2f, 0x3f: 0x3f,
+                   }
+
+        self.avl32 = AvalonMemory(dut, "master", dut.clk,
+                                  memory=self.memdict, readlatency_min=0)
+    @cocotb.coroutine
+    def init_sig(self, burstcount_w, address):
+        """ initialize all signals"""
+        yield Timer(10)
+        self.dut.reset = 0
+        self.dut.user_read_buffer = 0
+        self.dut.control_read_base = address
+        self.dut.control_read_length = burstcount_w*8
+        self.dut.control_fixed_location = 0
+        self.dut.control_go = 0
+        self.dut.master_waitrequest = 0
+
 
 @cocotb.test(expect_fail=False)
 def test_function_reentrant_clock(dut):
@@ -57,20 +100,47 @@ def test_function_reentrant_clock(dut):
 @cocotb.test(expect_fail=False)
 def test_burst_read(dut):
     """ Testing burst read """
-    # Launch clock
-    dut.reset = 1
-    clk_gen = cocotb.fork(Clock(dut.clk, 10).start())
+    wordburstcount = 4
+    address = 4
 
-    memdict = {}
-    avl32 = AvalonMemory(dut, "master", dut.clk)
-    yield Timer(10)
-    dut.reset = 0
-    dut.control_read_base = 0
-    dut.control_read_length = 32
-    dut.control_fixed_location = 0
-    dut.control_go = 0
-    dut.master_waitrequest = 0
+    bart = BurstAvlReadTest(dut)
+    yield bart.init_sig(wordburstcount, address)
     yield Timer(100)
+    # Begin master burst read
     dut.control_go = 1
+    yield Timer(10)
+    dut.control_go = 0
+    yield Timer(200)
+
+    # read back values
+    dut.user_read_buffer = 1
+    yield RisingEdge(dut.clk)
+    read_mem = {}
+    databuswidthB = len(dut.master_byteenable)
+    for burst in range(wordburstcount):
+        yield RisingEdge(dut.clk)
+        value = dut.user_buffer_data.value
+        for i in range(databuswidthB):
+            read_mem[(address + burst)*databuswidthB + i] =\
+                    (value >> i*8)& 0xFF
+
+    print str(read_mem)
+
+    # checking values read
+    for key, value in read_mem.items():
+        memdictvalue = bart.memdict.get(key, None)
+        if memdictvalue != value:
+            if memdictvalue is None:
+                memdictvalue = "Error"
+            else:
+                memdictvalue = hex(memdictvalue)
+            raise TestFailure("Wrong value read in memory :" +
+                              " read_mem[" + hex(key) + "] = " +
+                              hex(value) + " must be " +
+                              memdictvalue)
+                              
+
+    yield Timer(1000)
+    dut.user_read_buffer = 0
     yield Timer(1000)
 
