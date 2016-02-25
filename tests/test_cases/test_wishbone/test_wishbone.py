aec3fa7a081506e2bc7aa4537fc1df8f07819d4f
chiggs <chris.higgs@potentialventures.com>
2016-02-25 09:08:58 +0000

    Historical work on test

--- tests/test_cases/test_wishbone/test_wishbone.py
+++ tests/test_cases/test_wishbone/test_wishbone.py
@@ -1,7 +1,18 @@
+import logging
+
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge
 from cocotb.drivers.wishbone import WishboneMaster
+
+class GPIOTB(object):
+
+    def __init__(self, dut, debug=False):
+        self.dut = dut
+        self.log = logging.getLogger("cocotb.gpiotb")
+
+        # 
+_signals = ["cyc", "stb", "we", "sel", "adr", "datwr", "datrd", "ack"]
 
 
 @cocotb.test()
