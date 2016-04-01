d9f475063f08f8c6e2ba6b965fde43e63dde85c6
Martin Zabel <martin.zabel@tu-dresden.de>
2016-04-01 13:36:02 +0200

    Added license and documentation.

--- examples/dff/hdl/dff.v
+++ examples/dff/hdl/dff.v
@@ -1,9 +1,29 @@
-// Copyright (c) 2016 Technische Universitaet Dresden, Germany
-// Chair for VLSI-Design, Diagnostic and Architecture
-// Author: Martin Zabel
-// All rights reserved.
+// =============================================================================
+// Authors:		Martin Zabel
+// 
+// Module:              A simple D-FF
+// 
+// Description:
+// ------------------------------------
+// A simple D-FF with an initial state of '0'.
 //
-// A simple D flip-flop
+// License:
+// =============================================================================
+// Copyright 2016 Technische Universitaet Dresden - Germany
+//		  Chair for VLSI-Design, Diagnostics and Architecture
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//		http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// =============================================================================
 
 module dff (c,d,q);
    input wire c, d;
@@ -11,7 +31,9 @@ module dff (c,d,q);
 
    always @(posedge c)
      begin
-	q <= #1 d;
+	// It is also possible to add an delay of less than one clock period 
+	// here.
+	q <= d;
      end
    
 endmodule // dff
--- examples/dff/hdl/dff.vhdl
+++ examples/dff/hdl/dff.vhdl
@@ -1,9 +1,29 @@
--- Copyright (c) 2016 Technische Universitaet Dresden, Germany
--- Chair for VLSI-Design, Diagnostic and Architecture
--- Author: Martin Zabel
--- All rights reserved.
+-- =============================================================================
+-- Authors:		Martin Zabel
+-- 
+-- Module:              A simple D-FF
+-- 
+-- Description:
+-- ------------------------------------
+-- A simple D-FF with an initial state of '0'.
 --
--- A simple D flip-flop
+-- License:
+-- =============================================================================
+-- Copyright 2016 Technische Universitaet Dresden - Germany
+--		  Chair for VLSI-Design, Diagnostics and Architecture
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+--		http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- =============================================================================
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -17,5 +37,6 @@ end entity dff;
 
 architecture rtl of dff is
 begin
+  -- It is also possible to add an delay of less than one clock period here.
   q <= d when rising_edge(c);
 end architecture rtl;
