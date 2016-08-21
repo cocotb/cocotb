74aff6332a34b4a34b977f1404cb172dbf8d497a
mciepluc <mciepluc@gmail.com>
2016-08-21 19:14:08 +0200

    added distributions in CRV mechanisms

--- cocotb/crv.py
+++ cocotb/crv.py
@@ -41,14 +41,19 @@ class Randomized(object):
     def __init__(self):
         self._problem = constraint.Problem()
         self._randVariables = {}
+        self._distributions = {}
     
     def addRand(self, var, domain):
         self._randVariables[var] = domain #add variable to the map
         self._problem.addVariable(var, domain)
+        self._distributions[var] = lambda _: 1 #uniform distribution
     
     def addConstraint(self, constraint):
         variables = inspect.getargspec(constraint).args
         self._problem.addConstraint(constraint, variables)
+        
+    def addDistribution(self, var, distribution):
+        self._distributions[var] = distribution
         
     def pre_randomize(self):
         pass
@@ -66,8 +71,7 @@ class Randomized(object):
         for cstr in constraints:
             variables = inspect.getargspec(cstr).args
             problem.addConstraint(cstr, variables)
-        solutions = problem.getSolutions()
-        solution = solutions[random.randint(0,len(solutions)-1)]
+        solution = self.resolve(problem)
         for var in self._randVariables:
             setattr(self, var, solution[var]) 
         self.post_randomize()
@@ -82,8 +86,27 @@ class Randomized(object):
         for cstr in constraints:
             variables = inspect.getargspec(cstr).args
             problem.addConstraint(cstr, variables)
-        solutions = problem.getSolutions()
-        solution = solutions[random.randint(0,len(solutions)-1)]
+        solution = self.resolve(problem)
         for var in self._randVariables:
             setattr(self, var, solution[var]) 
+            
+    def resolve(self, problem):
+        solutions = problem.getSolutions()
+        solution_weights = []
+        for sol in solutions: #take single solution
+            weight = 1.0
+            for var in self._randVariables:
+                weight = weight*self._distributions[var](sol[var]) #calculate weight of total solution
+            solution_weights.append(weight)
+        #numpy should be used instead...
+        #solution = numpy.random.choice(solutions,size=1,p=solution_weights) #pick weighted random
+        #if numpy not available
+        min_weight = min(weight for weight in solution_weights if weight > 0)
+        solution_weights = map (lambda x: int(x*(1/min_weight)), solution_weights) #convert weights to int
+        weighted_solutions = []
+        for x in range(len(solutions)):
+            weighted_solutions = weighted_solutions + \
+              [solutions[x] for _ in range(solution_weights[x])] #multiply each solution in list accordingly
+        return random.choice(weighted_solutions)
+        
     
