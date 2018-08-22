afad944f65ba98c48efb8615d8d422b6396af976
Jeroen van Straten <jeroen.van.straten@technolution.nl>
2018-08-22 11:26:55 +0200

    Add test case for simulation time passing between last function and external return

--- tests/test_cases/test_external/test_external.py
+++ tests/test_cases/test_external/test_external.py
@@ -237,6 +237,25 @@ def test_time_between_functions(dut):
     yield cocotb.external(test_ext_time_between_functions)(dut, period)
 
 
+def test_ext_instantaneous_return(dut):
+    yield_to_next_clock(dut)
+    return get_sim_time('ps')
+
+
+@cocotb.test(expect_fail=False)
+def test_instantaneous_return(dut):
+    """Test that the simulation time does not advance between the final
+    function call in the external and the calling coroutine continuing"""
+    period = 100
+    clk_gen = cocotb.fork(Clock(dut.clk, period).start())
+    yield Timer(10, 'ns')
+    ext_sim_time = yield cocotb.external(test_ext_instantaneous_return)(dut)
+    cur_sim_time = get_sim_time('ps')
+    if cur_sim_time != ext_sim_time:
+        raise TestFailure("Time reported does not match expected time %d ps != %d ps" %
+                            (cur_sim_time, ext_sim_time))
+
+
 @cocotb.test(expect_fail=False)
 def test_ext_call_return(dut):
     """Test ability to yeild on an external non cocotb coroutine decorated
