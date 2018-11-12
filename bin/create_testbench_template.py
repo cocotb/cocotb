e9418b29db3d7eb2c319502b95b14987e3432f35
Colin Marquardt <colin.marquardt@bosch-sensortec.com>
2018-11-13 00:34:14 +0100

    Clean up trailing whitespace.

--- bin/create_testbench_template.py
+++ bin/create_testbench_template.py
@@ -23,7 +23,7 @@ def create_testbench_template(dut):
     '''
     Generate a testbench template for the DUT.
     '''
-    
+
     yield Timer(0)  # needed to make this a valid coroutine
 
     input_ports_=[]
@@ -75,11 +75,11 @@ def create_testbench_template(dut):
             inout_ports_.remove(port_)
         except:
             pass
-            
+
     filename_ = os.getenv('COCOTB_TESTBENCH_TEMPLATE')
     if filename_ is None:
         filename_ = 'testbench_template'
-        
+
     with open(filename_+'.py', 'w') as f:
         f.write('''# This is a generated testbench template for Cocotb (https://cocotb.readthedocs.io)
 
@@ -118,7 +118,7 @@ def my_first_test(dut):
             f.write('    dut.%s <= 1\n' % (port_._name))
         if len(supply_ports_) == 0:
             f.write('    # dut.%s <= 1\n' % ('<port>'))
-            
+
         f.write('\n')
         f.write('    dut._log.info("Asserting and de-asserting reset(s) if any were identified (assuming active-low resets)")\n')
         for port_ in reset_ports_:
@@ -156,7 +156,7 @@ def my_first_test(dut):
             f.write('    dut.%s <= 0x0\n' % (port_._name.ljust(maxlen_)))
         if len(driveable_ports_) == 0:
             f.write('    # dut.%s <= 0x0\n' % ('<port>'))
-        
+
         f.write('\n')
         f.write('    dut._log.info("Waiting for some rising edges on all clock signals")\n')
         for port_ in clock_ports_:
@@ -168,7 +168,7 @@ def my_first_test(dut):
 
         f.write('\n')
         f.write('    # yield ReadOnly()  # wait until all events have executed for this timestep\n')
-        
+
         f.write('\n')
         f.write('    dut._log.info("Reading all DUT outputs and inouts if any are present")\n')
         f.write('    # can also use int(port._name)\n')
@@ -183,7 +183,7 @@ def my_first_test(dut):
             f.write("""    dut._log.info("%s = %%s" %% str(%s))\n""" % ((port_._name).ljust(maxlen_), (port_._name+'_val')))
         if len(readable_ports_) == 0:
             f.write("""    # dut._log.info("%s = %%s" %% str(%s))\n""" % (('<port>').ljust(maxlen_), ('<port>'+'_val')))
-        
+
         f.write('''
     dut._log.info("Waiting 10 ns...")
     yield Timer(10, units='ns')
