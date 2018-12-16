7de638ed739a080651c283cba66740cfb5bde30f
Philipp Wagner <mail@philipp-wagner.com>
2018-12-16 20:19:01 +0000

    slightly different approach

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,39 +1,40 @@
 #!/bin/bash
 set -e
+set -x
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    # Manually install Python
-    brew outdated pyenv || brew upgrade pyenv
-    # virtualenv doesn't work without pyenv knowledge. venv in Python 3.3
-    # doesn't provide Pip by default. So, use `pyenv-virtualenv <https://github.com/yyuu/pyenv-virtualenv/blob/master/README.md>`_.
-    brew install pyenv-virtualenv
-    pyenv install $PYTHON
-    # I would expect something like ``pyenv init; pyenv local $PYTHON`` or
-    # ``pyenv shell $PYTHON`` would work, but ``pyenv init`` doesn't seem to
-    # modify the Bash environment. ??? So, I hand-set the variables instead.
-    export PYENV_VERSION=$PYTHON
-    export PATH="/Users/travis/.pyenv/shims:${PATH}"
-    pyenv virtualenv venv
-    source venv/bin/activate
-    # A manual check that the correct version of Python is running.
-    python --version
+    # Install Python
+    wget -q https://github.com/praekeltfoundation/travis-pyenv/releases/download/0.4.0/setup-pyenv.sh
+    source setup-pyenv.sh
 
-    # icarus Verilog
+    # Install Icarus Verilog
     brew install icarus-verilog
 
-    pip install tox-travis flake8
+    # Install all other Python build/test dependencies
+    pip install tox-travis flake8 pytest
+
+    # Check installation
+    pytest --version
+    flake8 --version
 elif [[ $TRAVIS_OS_NAME == 'linux' ]]; then
+    # Install Icarus Verilog
     sudo apt-get -qq update
     sudo apt-get -qq install -y --no-install-recommends git gperf make autoconf g++ flex bison
+
     mkdir -p /tmp/iverilog
-    cd /tmp/iverilog
+    cd /tmp/iverilog;
     git clone https://github.com/steveicarus/iverilog.git --depth=1 --branch v${ICARUS_VERILOG_VERSION} .
     sh autoconf.sh
     ./configure
     make -s -j${MAKE_JOBS}
     sudo make -s install
 
-    pip install tox-travis flake8
+    # Install all other Python build/test dependencies
+    pip install tox-travis flake8 pytest
+
+    # Check installation
+    pytest --version
+    flake8 --version
 else
     echo Unknown OS $TRAVIS_OS_NAME, nothing to install! >&2
 fi
