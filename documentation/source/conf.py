60e990af1998f4b8ce4ed8beea091dc6fbce20b2
Colin Marquardt <cmarqu42@gmail.com>
2019-01-04 23:26:23 +0100

    Add sphinx.ext.imgconverter ext for SVG in PDF

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -33,6 +33,7 @@ extensions = [
     'sphinx.ext.todo', 
     'sphinx.ext.coverage', 
     'sphinx.ext.pngmath', 
+    'sphinx.ext.imgconverter', 
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
     ]
