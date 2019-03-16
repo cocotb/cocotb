06cc409b3719edfe017757559c1b224b96c05ee9
killruana <killruana@gmail.com>
2019-03-16 10:38:51 +0100

    As asked by themperek, rewrite the test

--- tests/test_cases/issue_857/issue_857.py
+++ tests/test_cases/issue_857/issue_857.py
@@ -1,11 +1,34 @@
 import cocotb
-from cocotb import triggers, result
 import cocotb.regression
-import types
+import cocotb.triggers
 
 
-@cocotb.test()
-def issue_857(dut):
-    """ cocotb.regression must be a module """
-    if not isinstance(cocotb.regression, types.ModuleType):
-        yield result.TestFailure("cocotb.regression is not a module")
+@cocotb.coroutine
+def test_adder(dut, val_a, val_b):
+    dut.a = val_a
+    dut.b = val_b
+    yield cocotb.triggers.Timer(10, 'ns')
+    assert dut.z == val_a + val_b
+
+
+# Try to instantiate the TestFactory class using it's full specifier name.
+#
+# In issue #857, a global variable named "regression" in the cocotb module hide
+# the module cocotb.regression, so the TestFactory class is not accessible with 
+# an import like
+#
+# >>> import cocotb.regression
+# >>> factory = cocotb.regression.FactoryManager()
+#
+# The class is still accessible by an import like
+#
+# >>> from cocotb.regression import TestFactory
+# >>> factory = TestFactory()
+#
+# but the discoverer of the bug prefers the former approach.
+#
+# And in general, it's probably a good idea to not have name conflicts ;)
+test_factory = cocotb.regression.TestFactory(test_adder)
+test_factory.add_option('val_a', range(16))
+test_factory.add_option('val_b', range(16))
+test_factory.generate_tests()
