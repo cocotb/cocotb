c0e323d3662caba833605b683b91562667f6d957
Philipp Wagner <mail@philipp-wagner.com>
2019-06-27 20:21:38 +0100

    Revert "Move non-inline functions out of headers"
    This reverts commit 578293e17509410c0024356f431d03009032c813.
    
    Backout for test failure.

--- cocotb/share/include/cocotb_utils.h
+++ cocotb/share/include/cocotb_utils.h
@@ -34,6 +34,9 @@
 extern "C" {
 #endif
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #define xstr(a) str(a)
 #define str(a) #a
 
@@ -42,8 +45,24 @@ extern void* utils_dyn_sym(void *handle, const char* sym_name);
 
 extern int is_python_context;
 
-void to_python(void);
-void to_simulator(void);
+void to_python(void) {
+    if (is_python_context) {
+        fprintf(stderr, "FATAL: We are calling up again\n");
+        exit(1);
+    }
+    ++is_python_context;
+    //fprintf(stderr, "INFO: Calling up to python %d\n", is_python_context);
+}
+
+void to_simulator(void) {
+    if (!is_python_context) {
+        fprintf(stderr, "FATAL: We have returned twice from python\n");
+        exit(1);
+    }
+
+    --is_python_context;
+    //fprintf(stderr, "INFO: Returning back to simulator %d\n", is_python_context);
+}
 
 #ifdef __cplusplus
 }
--- cocotb/share/lib/utils/cocotb_utils.c
+++ cocotb/share/lib/utils/cocotb_utils.c
@@ -39,25 +39,6 @@
 // Tracks if we are in the context of Python or Simulator
 int is_python_context = 0;
 
-void to_python(void) {
-    if (is_python_context) {
-        fprintf(stderr, "FATAL: We are calling up again\n");
-        exit(1);
-    }
-    ++is_python_context;
-    //fprintf(stderr, "INFO: Calling up to python %d\n", is_python_context);
-}
-
-void to_simulator(void) {
-    if (!is_python_context) {
-        fprintf(stderr, "FATAL: We have returned twice from python\n");
-        exit(1);
-    }
-
-    --is_python_context;
-    //fprintf(stderr, "INFO: Returning back to simulator %d\n", is_python_context);
-}
-
 void* utils_dyn_open(const char* lib_name)
 {
     void *ret = NULL;
