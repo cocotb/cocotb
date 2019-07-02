e554c9a65c2dc596a7cbd096ec7c09f1a893acc3
Philipp Wagner <mail@philipp-wagner.com>
2019-07-02 21:23:52 +0100

    cocotb-config: Fix style

--- cocotb/config.py
+++ cocotb/config.py
@@ -28,14 +28,17 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ###############################################################################
 
+import argparse
 import os
 import sys
-import cocotb
-import argparse
+
 import pkg_resources
+
+import cocotb
 
 
 class PrintAction(argparse.Action):
+
     def __init__(self, option_strings, dest, text=None, **kwargs):
         super(PrintAction, self).__init__(option_strings, dest, nargs=0, **kwargs)
         self.text = text
@@ -44,24 +47,37 @@ class PrintAction(argparse.Action):
         print(self.text)
         parser.exit()
 
-def main():
 
-    share_dir = os.path.join(os.path.dirname(cocotb.__file__),'share')
+def main():
+    share_dir = os.path.join(os.path.dirname(cocotb.__file__), 'share')
     prefix_dir = os.path.dirname(os.path.dirname(cocotb.__file__))
-    makefiles_dir = os.path.join(os.path.dirname(cocotb.__file__),'share', 'makefiles')
+    makefiles_dir = os.path.join(os.path.dirname(cocotb.__file__), 'share', 'makefiles')
     version = pkg_resources.get_distribution('cocotb').version
 
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('--prefix', help='echos the package-prefix of cocotb', action=PrintAction, text=prefix_dir)
-    parser.add_argument('--share', help='echos the package-share of cocotb', action=PrintAction, text=share_dir)
-    parser.add_argument('--makefiles', help='echos the package-makefiles of cocotb', action=PrintAction, text=makefiles_dir)
-    parser.add_argument('-v', '--version', help='echos version of cocotb', action=PrintAction, text=version)
+    parser.add_argument('--prefix',
+                        help='echos the package-prefix of cocotb',
+                        action=PrintAction,
+                        text=prefix_dir)
+    parser.add_argument('--share',
+                        help='echos the package-share of cocotb',
+                        action=PrintAction,
+                        text=share_dir)
+    parser.add_argument('--makefiles',
+                        help='echos the package-makefiles of cocotb',
+                        action=PrintAction,
+                        text=makefiles_dir)
+    parser.add_argument('-v', '--version',
+                        help='echos version of cocotb',
+                        action=PrintAction,
+                        text=version)
 
-    if len(sys.argv)==1:
+    if len(sys.argv) == 1:
         parser.print_help(sys.stderr)
         sys.exit(1)
 
     args = parser.parse_args()
+
 
 if __name__ == "__main__":
     main()
