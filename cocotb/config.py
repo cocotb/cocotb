9812222a78a1b21818e1a58b2fa1e0d7fed67e75
Eric Wieser <wieser.eric@gmail.com>
2019-11-19 20:37:10 +0000

    Store the version-number more simply
    This:
    * eliminates the `VERSION` environment variable
    * adds `cocotb.__version__`
    * changes `setup.py` and `conf.py` to read this version number
    
    Fixes gh-1077, closes gh-1185

--- cocotb/config.py
+++ cocotb/config.py
@@ -32,7 +32,6 @@ import os
 import sys
 import cocotb
 import argparse
-import pkg_resources
 
 
 class PrintAction(argparse.Action):
@@ -49,7 +48,7 @@ def main():
     share_dir = os.path.join(os.path.dirname(cocotb.__file__),'share')
     prefix_dir = os.path.dirname(os.path.dirname(cocotb.__file__))
     makefiles_dir = os.path.join(os.path.dirname(cocotb.__file__),'share', 'makefiles')
-    version = pkg_resources.get_distribution('cocotb').version
+    version = cocotb.__version__
 
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('--prefix', help='echos the package-prefix of cocotb', action=PrintAction, text=prefix_dir)
