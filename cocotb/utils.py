5907c0bbf128c0f474936b33005adaf02e3170bf
Colin Marquardt <cmarqu42@gmail.com>
2019-12-13 18:33:13 +0100

    Add COCOTB_ANSI_OUTPUT to doc of want_color_output
    Related to #1231.

--- cocotb/utils.py
+++ cocotb/utils.py
@@ -494,7 +494,10 @@ class lazy_property(object):
 
 
 def want_color_output():
-    """Return ``True`` if colored output is possible/requested and not running in GUI."""
+    """Return ``True`` if colored output is possible/requested and not running in GUI.
+    
+    Colored output can be explicitly requested by setting :envvar:`COCOTB_ANSI_OUTPUT` to  ``1``.
+    """
     want_color = sys.stdout.isatty()  # default to color for TTYs
     if os.getenv("COCOTB_ANSI_OUTPUT", default='0') == '1':
         want_color = True
