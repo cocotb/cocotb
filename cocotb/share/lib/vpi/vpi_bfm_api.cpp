01c60e587ad7cabf2260738c1b3a65f72ffac647
Matthew Ballance <matt.ballance@gmail.com>
2019-12-14 13:49:57 -0800

    Update cocotb/share/lib/vpi/vpi_bfm_api.cpp
    Co-Authored-By: Colin Marquardt <cmarqu42@gmail.com>

--- cocotb/share/lib/vpi/vpi_bfm_api.cpp
+++ cocotb/share/lib/vpi/vpi_bfm_api.cpp
@@ -331,7 +331,7 @@ void register_bfm_tf(void) {
     tf_data.user_data = 0;
     vpi_register_systf(&tf_data);
 
-    // cocotb_bfm_add_str_param
+    // cocotb_bfm_add_param_str
 
     // cocotb_bfm_end_msg
     tf_data.type = vpiSysTask;
