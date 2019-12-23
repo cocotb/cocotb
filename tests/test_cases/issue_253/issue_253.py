0f6dbb91b215b87497ac517624f0ce58d240da89
Eric Wieser <wieser.eric@gmail.com>
2019-12-23 18:24:08 +0000

    Fix syntax warning in python 3.8

--- tests/test_cases/issue_253/issue_253.py
+++ tests/test_cases/issue_253/issue_253.py
@@ -10,17 +10,17 @@ from cocotb.binary import BinaryValue
 def toggle_clock(dut):
     dut.clk = 0
     yield Timer(10)
-    if dut.clk.value.integer is not 0:
+    if dut.clk.value.integer != 0:
         raise TestFailure("Clock not set to 0 as expected")
     dut.clk = 1
     yield Timer(10)
-    if dut.clk.value.integer is not 1:
+    if dut.clk.value.integer != 1:
         raise TestFailure("Clock not set to 1 as expected")
 
 @cocotb.test()
 def issue_253_empty(dut):
     yield toggle_clock(dut)
-  
+
 @cocotb.test()
 def issue_253_none(dut):
     yield toggle_clock(dut)
