2ce1d878129915bcd5674f80a0374bd722a7c626
Matthew Ballance <matt.ballance@gmail.com>
2020-01-22 13:14:23 -0500

    Move to using win32 SetEnvironmentVariable function on Windows
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- cocotb/share/lib/embed/gpi_embed.c
+++ cocotb/share/lib/embed/gpi_embed.c
@@ -136,7 +136,11 @@ void embed_init_python(void)
 
     // Before loading Python, ensure that COCOTB_SIM is set
     if (!getenv("COCOTB_SIM") || !getenv("COCOTB_SIM")[0]) {
-    	  setenv("COCOTB_SIM", "1", 1);
+#if defined(_WIN32)
+        SetEnvironmentVariable("COCOTB_SIM", "1");
+#else
+        setenv("COCOTB_SIM", "1", 1);
+#endif
     }
 
     void * lib_handle = utils_dyn_open(PY_SO_LIB);
