74484049beaada5239d6b9539fa09014874f19e6
Matthew Ballance <matt.ballance@gmail.com>
2020-01-25 12:05:15 -0500

    ensure _LOG_SIM_PRECISION is actually assigned
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- cocotb/utils.py
+++ cocotb/utils.py
@@ -70,6 +70,8 @@ def get_sim_time(units=None):
     return result
 
 def get_precision():
+    global _LOG_SIM_PRECISION
+    
     if _LOG_SIM_PRECISION is None:
         _LOG_SIM_PRECISION = cocotb.simulator.get_precision()  # request once and cache
     return _LOG_SIM_PRECISION
