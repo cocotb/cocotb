f43535b9c6e460cf23019a32b0300a3f5bcf20bf
Kaleb Barrett <ktbarrett.connectflour@gmail.com>
2020-01-26 22:23:54 -0600

    Merge Python version dependent C code in simulator module

--- cocotb/share/lib/simulator/simulatormodule.c
+++ cocotb/share/lib/simulator/simulatormodule.c
@@ -1004,8 +1004,52 @@ static void add_module_constants(PyObject* simulator)
         fprintf(stderr, "Failed to add module constants!\n");
 }
 
-#if PY_MAJOR_VERSION >= 3
-#include "simulatormodule_python3.c"
-#else
-#include "simulatormodule_python2.c"
-#endif
+static PyObject *error_out(PyObject *m, PyObject *args)
+{
+    COCOTB_UNUSED(args);
+    struct module_state *st = GETSTATE(m);
+    PyErr_SetString(st->error, "something bad happened");
+    return NULL;
+}
+
+static int simulator_traverse(PyObject *m, visitproc visit, void *arg) {
+    Py_VISIT(GETSTATE(m)->error);
+    return 0;
+}
+
+static int simulator_clear(PyObject *m) {
+    Py_CLEAR(GETSTATE(m)->error);
+    return 0;
+}
+
+static struct PyModuleDef moduledef = {
+    PyModuleDef_HEAD_INIT,
+    MODULE_NAME,
+    NULL,
+    sizeof(struct module_state),
+    SimulatorMethods,
+    NULL,
+    simulator_traverse,
+    simulator_clear,
+    NULL
+};
+
+PyMODINIT_FUNC
+MODULE_ENTRY_POINT(void)
+{
+    PyObject* simulator;
+
+    simulator = PyModule_Create(&moduledef);
+
+    if (simulator == NULL) INITERROR;
+    struct module_state *st = GETSTATE(simulator);
+
+    st->error = PyErr_NewException(MODULE_NAME ".Error", NULL, NULL);
+    if (st->error == NULL) {
+        Py_DECREF(simulator);
+        INITERROR;
+    }
+
+    add_module_constants(simulator);
+    return simulator;
+}
