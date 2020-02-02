01de1210cb6a017e76b1a104068a4befd4abef49
Matthew Ballance <matt.ballance@gmail.com>
2020-02-02 14:50:53 -0500

    Correct reference to the SIM_NAME variable
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- cocotb/regression.py
+++ cocotb/regression.py
@@ -133,8 +133,7 @@ class RegressionManager(object):
             # Verilator without full VPI support (<= 4.022) will report that
             # it cannot obtain a root handle. This is okay, as long as the
             # user doesn't need to directly-access signals
-            global SIM_NAME
-            if SIM_NAME == "Verilator":
+            if cocotb.SIM_NAME == "Verilator":
                 self.log.info("Can not find Root Handle (%s). Design hierarchy will not be accessible" %
                       self._root_name)
             else:
