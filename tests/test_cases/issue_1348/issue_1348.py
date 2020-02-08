b4816803155db1c05f6337d58d6f8a9e7479d1dc
Ben Reynwar <ben@reynwar.net>
2020-02-08 13:07:18 -0700

    Add a test for killing coroutines.

---
+++ tests/test_cases/issue_1348/issue_1348.py
@@ -0,0 +1,58 @@
+import os
+import logging
+
+import cocotb
+from cocotb import triggers, outcomes
+from cocotb_test import run
+
+logger = logging.getLogger(__name__)
+
+
+@cocotb.coroutine
+async def clock(clock_signal, period=2, units='ns'):
+    assert period % 2 == 0
+    while True:
+        clock_signal <= 0
+        await triggers.Timer(period//2, units=units)
+        clock_signal <= 1
+        await triggers.Timer(period//2, units=units)
+
+
+@cocotb.coroutine
+async def killable(dut, state):
+    state['killed'] = True
+    state['cleanup'] = False
+    try:
+        await triggers.RisingEdge(dut.clk)
+        await triggers.RisingEdge(dut.clk)
+        # We expect this coroutine to get killed before the second clock edge.
+        state['killed'] = False
+    finally:
+        # We expect this to run after the coroutine is killed.
+        state['cleanup'] = True
+
+@cocotb.coroutine
+async def killer(dut, killable_task):
+    await triggers.RisingEdge(dut.clk)
+    killable_task.kill()
+
+
+@cocotb.coroutine
+async def allow_kill(coro):
+    try:
+        await coro
+    except outcomes.KilledError:
+        pass
+
+@cocotb.test()
+async def test(dut):
+    cocotb.fork(clock(dut.clk))
+    state = {}
+    pB = killable(dut, state)
+    pA = killer(dut, pB)
+    cocotb.fork(pA)
+    cocotb.fork(allow_kill(pB))
+    for i in range(3):
+        await triggers.RisingEdge(dut.clk)
+    assert state['killed']
+    assert state['cleanup']
