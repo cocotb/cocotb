2ae19ded8c0044ba599fb7cad61c56d0f4cf4f92
Kaleb Barrett <ktbarrett.connectflour@gmail.com>
2020-02-10 20:54:35 -0600

    Fix test for Issue#1279

--- tests/test_cases/issue_1279/issue_1279.py
+++ tests/test_cases/issue_1279/issue_1279.py
@@ -10,7 +10,7 @@ def test_sim_failure_a(dut):
     yield cocotb.triggers.RisingEdge(dut.clk)
 
 
-@cocotb.test(stage=2)
+@cocotb.test(expect_error=cocotb.result.SimFailure, stage=2)
 def test_sim_failure_b(dut):
     yield cocotb.triggers.NullTrigger()
     raise cocotb.result.TestFailure("This test should never run")
