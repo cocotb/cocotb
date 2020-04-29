022bf0b86b66e629502b7e4751982659cf6a7d98
Tomasz Hemperek <themperek@users.noreply.github.com>
2020-04-29 17:28:03 +0200

    Update conf.py

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -12,11 +12,6 @@ import os
 import subprocess
 import sys
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../..'))
-
 # Add in-tree extensions to path
 sys.path.insert(0, os.path.abspath('../sphinxext'))
 
