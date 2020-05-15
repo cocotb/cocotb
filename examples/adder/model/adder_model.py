36c10943f8e58449fb4ebbac968883572ac3fc19
Kaleb Barrett <ktbarrett.connectflour@gmail.com>
2020-05-14 22:12:04 -0500

    Update adder example test and model

--- examples/adder/model/adder_model.py
+++ examples/adder/model/adder_model.py
@@ -1,3 +1,7 @@
-def adder_model(a, b):
+# This file is public domain, it can be freely copied without restrictions.
+# SPDX-License-Identifier: CC0-1.0
+
+
+def adder_model(a: int, b: int) -> int:
     """ model of adder """
     return a + b
