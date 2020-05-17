b54acce7e1879c49d18dd10ea4499b24a4a3b653
elgorwi <elgorwi@hotmail.com>
2020-05-17 17:26:04 +1000

    Issue #1829: Fix ImportError from cocotb import simulator

--- cocotb/_os_compat.py
+++ cocotb/_os_compat.py
@@ -14,8 +14,6 @@ import sys
 extra_dll_dir = os.path.join(os.path.dirname(__file__), 'libs')
 
 if sys.platform == 'win32' and os.path.isdir(extra_dll_dir):
-    if sys.version_info >= (3, 8):
-        os.add_dll_directory(extra_dll_dir)
-    else:
-        os.environ.setdefault('PATH', '')
-        os.environ['PATH'] += os.pathsep + extra_dll_dir
+    # TODO[gh-1829]: investigate making os.add_dll_directory work here on Python 3.8
+    os.environ.setdefault('PATH', '')
+    os.environ['PATH'] += os.pathsep + extra_dll_dir
