a883f5482190405fb18898870bec0f78fe14d17f
Kaleb Barrett <dev.ktbarrett@gmail.com>
2020-06-05 00:36:36 -0500

    Initial coverage support

--- cocotb/regression.py
+++ cocotb/regression.py
@@ -261,6 +261,10 @@ class RegressionManager:
             self.log.info("Writing coverage data")
             self._cov.save()
             self._cov.html_report()
+        if cocotb._library_coverage is not None:
+            # TODO: move this once we have normal shutdown behavior to _sim_event
+            cocotb._library_coverage.stop()
+            cocotb._library_coverage.save()
 
         # Setup simulator finalization
         simulator.stop_simulator()
