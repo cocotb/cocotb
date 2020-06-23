743ca9fb0565aa08b0d7d37e07094ebc5c8e7c7b
Ludwig Rogiers <elgorwi@users.noreply.github.com>
2020-06-23 18:04:16 +1000

    Update mean example test (#1730) (#1902)
    - Cleanup and remove python 2 statements
    - Change to  'async def' syntax
    - Add 'mean_range_test'

--- examples/mean/tests/test_mean.py
+++ examples/mean/tests/test_mean.py
@@ -1,12 +1,9 @@
 # This file is public domain, it can be freely copied without restrictions.
 # SPDX-License-Identifier: CC0-1.0
 
-from __future__ import print_function
-from __future__ import division
-
 import cocotb
-from cocotb.triggers import Timer, RisingEdge, ReadOnly
-from cocotb.result import TestFailure
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, ReadOnly
 from cocotb.monitors import BusMonitor
 from cocotb.scoreboard import Scoreboard
 
@@ -20,73 +17,70 @@ class StreamBusMonitor(BusMonitor):
 
     _signals = ["valid", "data"]
 
-    @cocotb.coroutine
-    def _monitor_recv(self):
+    async def _monitor_recv(self):
         """Watch the pins and reconstruct transactions."""
 
         while True:
-            yield RisingEdge(self.clock)
-            yield ReadOnly()
+            await RisingEdge(self.clock)
+            await ReadOnly()
             if self.bus.valid.value:
                 self._recv(int(self.bus.data.value))
 
 
-@cocotb.coroutine
-def clock_gen(signal, period=10, units='ns'):
-    while True:
-        signal <= 0
-        yield Timer(period/2, units=units)
-        signal <= 1
-        yield Timer(period/2, units=units)
-
-
-@cocotb.coroutine
-def value_test(dut, num):
-    """Test n*num/n = num"""
-
-    data_width = int(dut.DATA_WIDTH.value)
-    bus_width = int(dut.BUS_WIDTH.value)
+async def value_test(dut, nums):
+    """Test sum(nums)/n"""
+    DATA_WIDTH = int(dut.DATA_WIDTH.value)
+    BUS_WIDTH = int(dut.BUS_WIDTH.value)
     dut._log.info('Detected DATA_WIDTH = %d, BUS_WIDTH = %d' %
-                  (data_width, bus_width))
+                  (DATA_WIDTH, BUS_WIDTH))
 
-    cocotb.fork(clock_gen(dut.clk, period=CLK_PERIOD_NS, units='ns'))
+    cocotb.fork(Clock(dut.clk, CLK_PERIOD_NS, units='ns').start())
 
     dut.rst <= 1
-    for i in range(bus_width):
+    for i in range(BUS_WIDTH):
         dut.i_data[i] <= 0
     dut.i_valid <= 0
-    yield RisingEdge(dut.clk)
-    yield RisingEdge(dut.clk)
+    await RisingEdge(dut.clk)
+    await RisingEdge(dut.clk)
     dut.rst <= 0
 
-    for i in range(bus_width):
-        dut.i_data[i] <= num
+    for i in range(BUS_WIDTH):
+        dut.i_data[i] <= nums[i]
     dut.i_valid <= 1
-    yield RisingEdge(dut.clk)
+    await RisingEdge(dut.clk)
     dut.i_valid <= 0
-    yield RisingEdge(dut.clk)
+    await RisingEdge(dut.clk)
     got = int(dut.o_data.value)
 
-    if got != num:
-        raise TestFailure(
-            'Mismatch detected: got %d, exp %d!' % (got, num))
+    exp = sum(nums) // BUS_WIDTH
+
+    assert got == exp, "Mismatch detected: got {}, expected {}!".format(got, exp)
 
 
 @cocotb.test()
-def mean_basic_test(dut):
+async def mean_basic_test(dut):
     """Test n*5/n = 5"""
-    yield value_test(dut, 5)
+    BUS_WIDTH = int(dut.BUS_WIDTH.value)
+    await value_test(dut, [5] * BUS_WIDTH)
 
 
 @cocotb.test()
-def mean_overflow_test(dut):
+async def mean_range_test(dut):
+    """Test range(n)/n"""
+    BUS_WIDTH = int(dut.BUS_WIDTH.value)
+    await value_test(dut, range(1, BUS_WIDTH + 1))
+
+
+@cocotb.test()
+async def mean_overflow_test(dut):
     """Test for overflow n*max_val/n = max_val"""
-    data_width = int(dut.DATA_WIDTH.value)
-    yield value_test(dut, 2**data_width - 1)
+    BUS_WIDTH = int(dut.BUS_WIDTH.value)
+    DATA_WIDTH = int(dut.DATA_WIDTH.value)
+    await value_test(dut, [2**DATA_WIDTH - 1] * BUS_WIDTH)
 
 
 @cocotb.test()
-def mean_randomised_test(dut):
+async def mean_randomised_test(dut):
     """Test mean of random numbers multiple times"""
 
     # dut_in = StreamBusMonitor(dut, "i", dut.clk)  # this doesn't work:
@@ -99,30 +93,33 @@ def mean_randomised_test(dut):
     scoreboard = Scoreboard(dut)
     scoreboard.add_interface(dut_out, exp_out)
 
-    data_width = int(dut.DATA_WIDTH.value)
-    bus_width = int(dut.BUS_WIDTH.value)
+    DATA_WIDTH = int(dut.DATA_WIDTH.value)
+    BUS_WIDTH = int(dut.BUS_WIDTH.value)
     dut._log.info('Detected DATA_WIDTH = %d, BUS_WIDTH = %d' %
-                  (data_width, bus_width))
+                  (DATA_WIDTH, BUS_WIDTH))
 
-    cocotb.fork(clock_gen(dut.clk, period=CLK_PERIOD_NS, units='ns'))
+    cocotb.fork(Clock(dut.clk, CLK_PERIOD_NS, units='ns').start())
 
     dut.rst <= 1
-    for i in range(bus_width):
+    for i in range(BUS_WIDTH):
         dut.i_data[i] = 0
     dut.i_valid <= 0
-    yield RisingEdge(dut.clk)
-    yield RisingEdge(dut.clk)
+    await RisingEdge(dut.clk)
+    await RisingEdge(dut.clk)
     dut.rst <= 0
 
     for j in range(10):
         nums = []
-        for i in range(bus_width):
-            x = random.randint(0, 2**data_width - 1)
+        for i in range(BUS_WIDTH):
+            x = random.randint(0, 2**DATA_WIDTH - 1)
             dut.i_data[i] = x
             nums.append(x)
         dut.i_valid <= 1
 
-        nums_mean = sum(nums) // bus_width
+        nums_mean = sum(nums) // BUS_WIDTH
         exp_out.append(nums_mean)
-        yield RisingEdge(dut.clk)
+        await RisingEdge(dut.clk)
         dut.i_valid <= 0
+
+    await RisingEdge(dut.clk)
+    await RisingEdge(dut.clk)
