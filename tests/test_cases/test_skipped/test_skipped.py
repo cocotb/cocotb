1a51ba7d64a4bbbbcb55135e4c5eeb646bb5dde7
Ben Rosser <rosser.bjr@gmail.com>
2020-09-04 16:15:48 -0400

    Add test that skip=True tests can be ran by setting TESTCASE
    The test creates a temporay file, "ran_skipped_test~" (I gave it the
    vim temporary file ending so that it would already be covered by
    the cocotb gitignore), when it is ran, and does nothing else. The
    test has skip=True set in its constructor.
    
    The Makefile for this test then sets ```TESTCASE=test_skipped``` to
    ensure that the test is ran, and overrides the default make target so
    that we can test if the file was created. If the file wasn't created,
    then we exit with status code 1.

---
+++ tests/test_cases/test_skipped/test_skipped.py
@@ -0,0 +1,15 @@
+# Copyright cocotb contributors
+# Licensed under the Revised BSD License, see LICENSE for details.
+# SPDX-License-Identifier: BSD-3-Clause
+
+import pathlib
+
+import cocotb
+
+skipped_file_name = "ran_skipped_test~"
+
+
+@cocotb.test(skip=True)
+async def test_skipped(dut):
+    """ Touch a file so we can check that this test has ran."""
+    pathlib.Path(skipped_file_name).touch()
