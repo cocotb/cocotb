ae8a7ecd8f6c37c9328ff6c8ce04c1161db7633e
Kaleb Barrett <dev.ktbarrett@gmail.com>
2020-10-19 21:12:18 -0500

    Add test of $fatal cleaning shutting down cocotb
    $fatal stops the simulator. This should cause cocotb's regression
    manager to record failure for the current and all remaining sims.

---
+++ tests/test_cases/test_fatal/fatal.vhd
@@ -0,0 +1,18 @@
+-- Copyright cocotb contributors
+-- Licensed under the Revised BSD License, see LICENSE for details.
+-- SPDX-License-Identifier: BSD-3-Clause USE OF THIS
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+entity fatal is
+end entity fatal;
+
+architecture behav of fatal is
+begin
+
+	fatal_proc : process is
+	begin
+		wait for 10 ns;
+		report "This is a fatal message that finishes the test" severity FAILURE;
+	end process fatal_proc;
+
+end architecture behav;
