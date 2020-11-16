59be8d57a54ec36ed0615cc4dadf6254beaa0e4d
Kaleb Barrett <dev.ktbarrett@gmail.com>
2020-11-16 00:34:01 -0600

    Add test for a test failure that isn't expected

---
+++ tests/test_cases/test_failure/test_failure.py
@@ -0,0 +1,15 @@
+# Copyright cocotb contributors
+# Licensed under the Revised BSD License, see LICENSE for details.
+# SPDX-License-Identifier: BSD-3-Clause
+"""
+Tests the failure path for tests in the regression.py and xunit_reporter.py.
+
+The Makefile in the same repo is specially set up to squash any error code due
+to a failing test and ensures the failing test is reported properly.
+"""
+import cocotb
+
+
+@cocotb.test()
+async def test_fail():
+    assert False
