7873b2aac2da3479934fa5323dc456c6fd55d5d6
Kaleb Barrett <dev.ktbarrett@gmail.com>
2020-11-22 11:46:21 -0600

    Increase coverage in xunit_reporter.py (#2200)
    * Add test for a test failure that isn't expected
    * Remove dead code in xunit_reporter.py
    
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- cocotb/xunit_reporter.py
+++ cocotb/xunit_reporter.py
@@ -28,40 +28,6 @@
 from xml.etree.ElementTree import Element, SubElement
 import xml.etree.ElementTree as ET
 
-import mmap
-from io import StringIO
-
-TRUNCATE_LINES = 100
-
-
-# file from  http://stackoverflow.com/questions/136168/get-last-n-lines-of-a-file-with-python-similar-to-tail
-class File(StringIO):
-
-    def countlines(self):
-        buf = mmap.mmap(self.fileno(), 0)
-        lines = 0
-        while buf.readline():
-            lines += 1
-        return lines
-
-    def head(self, lines_2find=1):
-        self.seek(0)                            # Rewind file
-        return [self.next() for x in range(lines_2find)]
-
-    def tail(self, lines_2find=1):
-        self.seek(0, 2)                         # go to end of file
-        bytes_in_file = self.tell()
-        lines_found, total_bytes_scanned = 0, 0
-        while (lines_2find+1 > lines_found and
-               bytes_in_file > total_bytes_scanned):
-            byte_block = min(1024, bytes_in_file-total_bytes_scanned)
-            self.seek(-(byte_block+total_bytes_scanned), 2)
-            total_bytes_scanned += byte_block
-            lines_found += self.read(1024).count('\n')
-        self.seek(-total_bytes_scanned, 2)
-        line_list = list(self.readlines())
-        return line_list[-lines_2find:]
-
 
 class XUnitReporter:
 
@@ -84,30 +50,6 @@ class XUnitReporter:
             testsuite = self.last_testsuite
         self.last_property = SubElement(testsuite, "property", **kwargs)
         return self.last_property
-
-    def update_testsuite(self, testsuite=None, **kwargs):
-        if testsuite is None:
-            testsuite = self.last_testsuite
-        for k in kwargs:
-            testsuite.set(k, str(kwargs[k]))
-
-    def update_testsuites(self, **kwargs):
-        for k in kwargs:
-            self.results.set(k, str(kwargs[k]))
-
-    def add_log(self, logfile, testcase=None):
-        if testcase is None:
-            testcase = self.last_testcase
-        log = SubElement(testcase, "system-out")
-        f = File(logfile, 'r+')
-        lines = f.countlines()
-        if lines > (TRUNCATE_LINES * 2):
-            head = f.head(TRUNCATE_LINES)
-            tail = f.tail(TRUNCATE_LINES)
-            log.text = "".join(head + list("[...truncated %d lines...]\n" %
-                               ((lines - (TRUNCATE_LINES*2)))) + tail)
-        else:
-            log.text = "".join(f.readlines())
 
     def add_failure(self, testcase=None, **kwargs):
         if testcase is None:
