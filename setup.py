5e82cf86b8395166511f0a3fc2c7b5cd08142691
Kaleb Barrett <dev.ktbarrett@gmail.com>
2020-12-13 17:49:20 -0600

    Add cocotb[bus] extras which also installs cocotb_bus

--- setup.py
+++ setup.py
@@ -133,6 +133,10 @@ setup(
         "Source Code": "https://github.com/cocotb/cocotb",
         "Documentation": "https://docs.cocotb.org",
     },
+
+    extras_require={
+        "bus": ["cocotb_bus"]
+    }
 )
 
 print(log_stream.getvalue())
