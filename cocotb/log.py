5b56c02cb873e98cc1c550ffde9d5f7e545d2546
raysalemi <ray@raysalemi.com>
2021-01-31 12:11:36 -0500

    Let colorizer handle non-standard log levels
    If a user defines a log level the colorizer used to raise a KeyError
    Now it simply doesn't provide a color as it does with DEBUG level.
    I accessed the dictionary using get()
    
    Fixes #2362

--- cocotb/log.py
+++ cocotb/log.py
@@ -248,8 +248,8 @@ class SimColourLogFormatter(SimLogFormatter):
         msg = record.getMessage()
 
         # Need to colour each line in case coloring is applied in the message
-        msg = '\n'.join([SimColourLogFormatter.loglevel2colour[record.levelno] % line for line in msg.split('\n')])
-        level = (SimColourLogFormatter.loglevel2colour[record.levelno] %
+        msg = '\n'.join([SimColourLogFormatter.loglevel2colour.get(record.levelno,"%s") % line for line in msg.split('\n')])
+        level = (SimColourLogFormatter.loglevel2colour.get(record.levelno, "%s") %
                  record.levelname.ljust(_LEVEL_CHARS))
 
         return self._format(level, record, msg, coloured=True)
