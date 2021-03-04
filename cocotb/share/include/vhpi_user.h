3eeb48d6cd3c54b707466a489968b4bb9944c062
Marlon James <marlon.james@gmail.com>
2021-03-03 17:42:09 -0800

    Explicitly include stdint.h on MacOS
    clang-format re-arranged order of header includes,
    exposing a transitive include issue. On MacOS, both
    vpi_user.h and vhpi_user.h do not include stdint.h
    so do not have type definitions for uint*_t and
    int*_t types.
    
    This modifies the files to explicitly include
    stdint.h on MacOS.

--- cocotb/share/include/vhpi_user.h
+++ cocotb/share/include/vhpi_user.h
@@ -29,7 +29,7 @@
     typedef signed __int64 int64_t;
     typedef signed __int32 int32_t;
     typedef signed __int8 int8_t;
-#elif defined(__MINGW32__)
+#elif defined(__MINGW32__) || (defined(__APPLE__) && defined(__MACH__))
     #include <stdint.h>
 #elif defined(__linux)
     #include <inttypes.h>
