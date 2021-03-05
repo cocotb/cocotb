7d3a4e856e2edaf8952914a9b48a8aa3f2c72b86
Kaleb Barrett <dev.ktbarrett@gmail.com>
2021-03-04 22:31:01 -0600

    Remove cocotb-bus stubs

--- cocotb/monitors/xgmii.py
+++
@@ -1,5 +0,0 @@
-# Copyright cocotb contributors
-# Licensed under the Revised BSD License, see LICENSE for details.
-# SPDX-License-Identifier: BSD-3-Clause
-
-from cocotb_bus.monitors.xgmii import XGMII  # noqa: F401
