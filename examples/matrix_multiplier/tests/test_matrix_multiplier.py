bf0621cc6db12fea00e8bd3bfb802bd06af35582
Kaleb Barrett <dev.ktbarrett@gmail.com>
2021-03-05 19:27:16 -0600

    Update matrix multiplier example
    Removes dependency on cocotb_bus. Changes testbench to use Queues and
    design for reuse.

--- examples/matrix_multiplier/tests/test_matrix_multiplier.py
+++ examples/matrix_multiplier/tests/test_matrix_multiplier.py
@@ -4,14 +4,15 @@
 import math
 import os
 from random import getrandbits
+from typing import Dict, List, Any
 
 import cocotb
 from cocotb.binary import BinaryValue
 from cocotb.clock import Clock
 from cocotb.regression import TestFactory
-from cocotb.triggers import RisingEdge, ReadOnly
-
-from cocotb_bus.monitors import BusMonitor
+from cocotb.triggers import RisingEdge
+from cocotb.queue import Queue
+from cocotb.handle import SimHandleBase
 
 NUM_SAMPLES = int(os.environ.get('NUM_SAMPLES', 3000))
 DATA_WIDTH = int(cocotb.top.DATA_WIDTH)
@@ -20,83 +21,131 @@ B_COLUMNS = int(cocotb.top.B_COLUMNS)
 A_COLUMNS_B_ROWS = int(cocotb.top.A_COLUMNS_B_ROWS)
 
 
-class MatrixMonitor(BusMonitor):
-    """Base class for monitoring inputs/outputs of Matrix Multiplier."""
-    def __init__(self, dut, callback=None, event=None):
-        super().__init__(dut, "", dut.clk_i, callback=callback, event=event)
-
-
-class MatrixInMonitor(MatrixMonitor):
-    """Monitor inputs to Matrix Multiplier module.
-
-    Generate expected results for each multiplication operation.
+class DataValidMonitor:
     """
-    _signals = {"A": "a_i", "B": "b_i", "valid": "valid_i"}
+    Reusable Monitor of one-way control flow (data/valid) streaming data interface
 
-    async def _monitor_recv(self):
-        while True:
-            await RisingEdge(self.clock)
-            await ReadOnly()
-
-            if self.bus.valid.value:
-                a_matrix = self.bus.A.value
-                b_matrix = self.bus.B.value
-
-                # Calculate the expected result of C
-                c_expected = [
-                    BinaryValue(
-                        sum(
-                            [
-                                a_matrix[(i * A_COLUMNS_B_ROWS) + n] * b_matrix[(n * B_COLUMNS) + j]
-                                for n in range(A_COLUMNS_B_ROWS)
-                            ]
-                        ),
-                        n_bits=(DATA_WIDTH * 2) + math.ceil(math.log2(A_COLUMNS_B_ROWS)),
-                        bigEndian=False
-                    )
-                    for i in range(A_ROWS)
-                    for j in range(B_COLUMNS)
-                ]
-
-                self._recv(c_expected)
-
-
-class MatrixOutMonitor(MatrixMonitor):
-    """Monitor outputs from Matrix Multiplier module.
-
-    Capture result matrix for each multiplication operation.
+    Args
+        clk: clock signal
+        valid: control signal noting a transaction occured
+        datas: named handles to be sampled when transaction occurs
     """
-    _signals = {"C": "c_o", "valid": "valid_o"}
 
-    async def _monitor_recv(self):
+    def __init__(self, clk: SimHandleBase, datas: Dict[str, SimHandleBase], valid: SimHandleBase):
+        self.values = Queue[Dict[str, int]]()
+        self._clk = clk
+        self._datas = datas
+        self._valid = valid
+        self._coro = None
+
+    def start(self) -> None:
+        """Start monitor"""
+        if self._coro is not None:
+            raise RuntimeError("Monitor already started")
+        self._coro = cocotb.fork(self._run())
+
+    def stop(self) -> None:
+        """Stop monitor"""
+        if self._coro is None:
+            raise RuntimeError("Monitor never started")
+        self._coro.kill()
+        self._coro = None
+
+    async def _run(self) -> None:
         while True:
-            await RisingEdge(self.clock)
-            await ReadOnly()
+            await RisingEdge(self._clk)
+            if self._valid.value.binstr != '1':
+                await RisingEdge(self._valid)
+                continue
+            self.values.put_nowait(self._sample())
 
-            if self.bus.valid.value:
-                c_actual = self.bus.C.value
-                self._recv(c_actual)
+    def _sample(self) -> Dict[str, Any]:
+        """
+        Samples the data signals and builds a transaction object
+
+        Return value is what is stored in queue. Meant to be overriden by the user.
+        """
+        return {name: handle.value for name, handle in self._datas.items()}
+
+
+class MatrixMultiplierTester:
+    """
+    Reusable checker of a matrix_multiplier instance
+
+    Args
+        matrix_multiplier_entity: handle to an instance of matrix_multiplier
+    """
+
+    def __init__(self, matrix_multiplier_entity: SimHandleBase):
+        self.dut = matrix_multiplier_entity
+
+        self.input_mon = DataValidMonitor(
+            clk=self.dut.clk_i,
+            valid=self.dut.valid_i,
+            datas=dict(
+                A=self.dut.a_i,
+                B=self.dut.b_i))
+
+        self.output_mon = DataValidMonitor(
+            clk=self.dut.clk_i,
+            valid=self.dut.valid_o,
+            datas=dict(C=self.dut.c_o))
+
+        self._checker = None
+
+    def start(self) -> None:
+        """Starts monitors, model, and checker coroutine"""
+        if self._checker is not None:
+            raise RuntimeError("Monitor already started")
+        self.input_mon.start()
+        self.output_mon.start()
+        self._checker = cocotb.fork(self._check())
+
+    def stop(self) -> None:
+        """Stops everything"""
+        if self._checker is None:
+            raise RuntimeError("Monitor never started")
+        self.input_mon.stop()
+        self.output_mon.stop()
+        self._checker.kill()
+        self._checker = None
+
+    def model(self, a_matrix: List[int], b_matrix: List[int]) -> List[int]:
+        """Transaction-level model of the matrix multipler as instantiated"""
+        A_ROWS = self.dut.A_ROWS.value
+        A_COLUMNS_B_ROWS = self.dut.A_COLUMNS_B_ROWS.value
+        B_COLUMNS = self.dut.B_COLUMNS.value
+        DATA_WIDTH = self.dut.DATA_WIDTH.value
+        return [
+            BinaryValue(
+                sum(
+                    [
+                        a_matrix[(i * A_COLUMNS_B_ROWS) + n] * b_matrix[(n * B_COLUMNS) + j]
+                        for n in range(A_COLUMNS_B_ROWS)
+                    ]
+                ),
+                n_bits=(DATA_WIDTH * 2) + math.ceil(math.log2(A_COLUMNS_B_ROWS)),
+                bigEndian=False
+            )
+            for i in range(A_ROWS)
+            for j in range(B_COLUMNS)
+        ]
+
+    async def _check(self) -> None:
+        while True:
+            actual = await self.output_mon.values.get()
+            expected_inputs = await self.input_mon.values.get()
+            expected = self.model(
+                a_matrix=expected_inputs['A'],
+                b_matrix=expected_inputs['B'])
+            assert actual['C'] == expected
 
 
 async def test_multiply(dut, a_data, b_data):
     """Test multiplication of many matrices."""
 
     cocotb.fork(Clock(dut.clk_i, 10, units='ns').start())
-
-    # Configure checker to compare module results to expected
-    expected_output = []
-
-    dut._log.info("Configure monitors")
-
-    in_monitor = MatrixInMonitor(dut, callback=expected_output.append)
-
-    def check_output(transaction):
-        if not expected_output:
-            raise RuntimeError("Monitor captured unexpected multiplication operation")
-        exp = expected_output.pop(0)
-        assert transaction == exp, "Multiplication result {} did not match expected result {}".format(transaction, exp)
-
-    out_monitor = MatrixOutMonitor(dut, callback=check_output)
+    tester = MatrixMultiplierTester(dut)
 
     dut._log.info("Initialize and reset model")
 
@@ -110,6 +159,9 @@ async def test_multiply(dut, a_data, b_data):
     for _ in range(3):
         await RisingEdge(dut.clk_i)
     dut.reset_i <= 0
+
+    # start tester after reset so we know it's in a good state
+    tester.start()
 
     dut._log.info("Test multiplication operations")
 
