d055523a961ef42bf0761cf261dd933dc9040f5d
Martijn Bastiaan <martijn@hmbastiaan.nl>
2021-03-12 12:16:53 +0100

    Add VHDL library support to Modelsim/Questa

---
+++ tests/test_cases/test_vhdl_libraries/test_ab.py
@@ -0,0 +1,11 @@
+import cocotb
+
+
+@cocotb.test()
+async def test(dut):
+    # Toggling an input should trigger the simulator to print a message
+    # similar to:
+    #
+    #   b.vhdl:9:5:@0ms:(report note): :a(structural):b@b(structural):
+    #
+    dut.x = False
