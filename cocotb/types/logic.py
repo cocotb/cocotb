9053ab33140c4c55b8bff67ce957ce825023f4a3
Kaleb Barrett <dev.ktbarrett@gmail.com>
2021-03-21 16:03:13 -0500

    Add license headers

--- cocotb/types/logic.py
+++ cocotb/types/logic.py
@@ -1,3 +1,6 @@
+# Copyright cocotb contributors
+# Licensed under the Revised BSD License, see LICENSE for details.
+# SPDX-License-Identifier: BSD-3-Clause
 from cocotb._py_compat import cache
 
 
--- tests/pytest/test_logic.py
+++ tests/pytest/test_logic.py
@@ -1,3 +1,6 @@
+# Copyright cocotb contributors
+# Licensed under the Revised BSD License, see LICENSE for details.
+# SPDX-License-Identifier: BSD-3-Clause
 from cocotb.types import Logic, Bit
 import pytest
 
