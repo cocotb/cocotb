3dd2c1317debaaa82aad3f5d22770c0ab34df085
Kaleb Barrett <dev.ktbarrett@gmail.com>
2021-08-20 18:54:50 -0500

    Make Python 3.6 and 3.7 work

--- tests/test_cases/test_forked_exception/test_forked_exception.py
+++ tests/test_cases/test_forked_exception/test_forked_exception.py
@@ -7,8 +7,13 @@ Tests the failure path for tests in the regression.py and xunit_reporter.py.
 The Makefile in this folder is specially set up to squash any error code due
 to a failing test and ensures the failing test is reported properly.
 """
+import warnings
+
 import cocotb
 from cocotb.triggers import Timer
+
+
+warnings.simplefilter("error", category=FutureWarning)
 
 
 class MyException(Exception):
