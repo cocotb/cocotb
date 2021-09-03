9d4d9fbb06e78be99fc5d50afef934d8796707ea
Kaleb Barrett <dev.ktbarrett@gmail.com>
2021-09-03 00:12:26 -0500

    Significant cleanup to test_multi_dimension_array

--- tests/test_cases/test_multi_dimension_array/test_cocotb_array.py
+++ tests/test_cases/test_multi_dimension_array/test_cocotb_array.py
@@ -1,227 +1,169 @@
 import cocotb
-from cocotb.result import TestFailure
 from cocotb.triggers import Timer
 
 
 @cocotb.test()
 async def test_in_vect_packed(dut):
+    test_value = 0x5
+    dut.in_vect_packed.value = test_value
     await Timer(1, "ns")
-    print("Setting: dut.in_vect_packed type %s" % type(dut.in_vect_packed))
-    dut.in_vect_packed = 0x5
-    await Timer(1, "ns")
-    print("Getting: dut.out_vect_packed type %s" % type(dut.out_vect_packed))
-    if dut.out_vect_packed != 0x5:
-        raise TestFailure("Failed to readback dut.out_vect_packed")
+    assert dut.out_vect_packed.value == test_value
 
 
 @cocotb.test()
 async def test_in_vect_unpacked(dut):
+    test_value = [0x1, 0x0, 0x1]
+    dut.in_vect_unpacked.value = test_value
     await Timer(1, "ns")
-    print("Setting: dut.in_vect_unpacked type %s" % type(dut.in_vect_unpacked))
-    dut.in_vect_unpacked = [0x1, 0x0, 0x1]
-    await Timer(1, "ns")
-    print("Getting: dut.out_vect_unpacked type %s" % type(dut.out_vect_unpacked))
-    if dut.out_vect_unpacked != [0x1, 0x0, 0x1]:
-        raise TestFailure("Failed to readback dut.out_vect_unpacked")
+    assert dut.out_vect_unpacked.value == test_value
 
 
 @cocotb.test()
 async def test_in_arr(dut):
+    test_value = 0x5
+    dut.in_arr.value = test_value
     await Timer(1, "ns")
-    print("Setting: dut.in_arr type %s" % type(dut.in_arr))
-    dut.in_arr = 0x5
-    await Timer(1, "ns")
-    print("Getting: dut.out_arr type %s" % type(dut.out_arr))
-    if dut.out_arr != 0x5:
-        raise TestFailure("Failed to readback dut.out_arr")
+    assert dut.out_arr.value == test_value
 
 
 @cocotb.test()
 async def test_in_2d_vect_packed_packed(dut):
+    test_value = (0x5 << 6) | (0x5 << 3) | 0x5
+    dut.in_2d_vect_packed_packed.value = test_value
     await Timer(1, "ns")
-    print("Setting: dut.in_2d_vect_packed_packed type %s" % type(dut.in_2d_vect_packed_packed))
-    dut.in_2d_vect_packed_packed = (0x5 << 6) | (0x5 << 3) | 0x5
-    await Timer(1, "ns")
-    print("Getting: dut.out_2d_vect_packed_packed type %s" % type(dut.out_2d_vect_packed_packed))
-    if dut.out_2d_vect_packed_packed != (0x5 << 6) | (0x5 << 3) | 0x5:
-        raise TestFailure("Failed to readback dut.out_2d_vect_packed_packed")
+    assert dut.out_2d_vect_packed_packed.value == test_value
 
 
 @cocotb.test()
 async def test_in_2d_vect_packed_unpacked(dut):
+    test_value = [0x5, 0x5, 0x5]
+    dut.in_2d_vect_packed_unpacked.value = test_value
     await Timer(1, "ns")
-    print("Setting: dut.in_2d_vect_packed_unpacked type %s" % type(dut.in_2d_vect_packed_unpacked))
-    dut.in_2d_vect_packed_unpacked = [0x5, 0x5, 0x5]
-    await Timer(1, "ns")
-    print("Getting: dut.out_2d_vect_packed_unpacked type %s" % type(dut.out_2d_vect_packed_unpacked))
-    if dut.out_2d_vect_packed_unpacked != [0x5, 0x5, 0x5]:
-        raise TestFailure("Failed to readback dut.out_2d_vect_packed_unpacked")
+    assert dut.out_2d_vect_packed_unpacked.value == test_value
 
 
 @cocotb.test()
 async def test_in_2d_vect_unpacked_unpacked(dut):
+    test_value = 3*[[0x1, 0x0, 0x1]]
+    dut.in_2d_vect_unpacked_unpacked.value = test_value
     await Timer(1, "ns")
-    print("Setting: dut.in_2d_vect_unpacked_unpacked type %s" % type(dut.in_2d_vect_unpacked_unpacked))
-    dut.in_2d_vect_unpacked_unpacked = 3*[[0x1, 0x0, 0x1]]
-    await Timer(1, "ns")
-    print("Getting: dut.out_2d_vect_unpacked_unpacked type %s" % type(dut.out_2d_vect_unpacked_unpacked))
-    if dut.out_2d_vect_unpacked_unpacked != 3*[[0x1, 0x0, 0x1]]:
-        raise TestFailure("Failed to readback dut.out_2d_vect_unpacked_unpacked")
+    assert dut.out_2d_vect_unpacked_unpacked.value == test_value
 
 
 @cocotb.test()
 async def test_in_arr_packed(dut):
+    test_value = 365
+    dut.in_arr_packed.value = test_value
     await Timer(1, "ns")
-    print("Setting: dut.in_arr_packed type %s" % type(dut.in_arr_packed))
-    dut.in_arr_packed = 365
-    await Timer(1, "ns")
-    print("Getting: dut.out_arr_packed type %s" % type(dut.out_arr_packed))
-    if dut.out_arr_packed != 365:
-        raise TestFailure("Failed to readback dut.out_arr_packed")
+    assert dut.out_arr_packed.value == test_value
 
 
 @cocotb.test()
 async def test_in_arr_unpacked(dut):
+    test_value = [0x5, 0x5, 0x5]
+    dut.in_arr_unpacked.value = test_value
     await Timer(1, "ns")
-    print("Setting: dut.in_arr_unpackedtype %s" % type(dut.in_arr_unpacked))
-    dut.in_arr_unpacked = [0x5, 0x5, 0x5]
-    await Timer(1, "ns")
-    print("Getting: dut.out_arr_unpackedtype %s" % type(dut.out_arr_unpacked))
-    if dut.out_arr_unpacked != [0x5, 0x5, 0x5]:
-        raise TestFailure("Failed to readback dut.out_arr_unpacked")
+    assert dut.out_arr_unpacked.value == test_value
 
 
 @cocotb.test()
 async def test_in_2d_arr(dut):
+    test_value = 365
+    dut.in_2d_arr.value = test_value
     await Timer(1, "ns")
-    print("Setting: dut.in_2d_arr type %s" % type(dut.in_2d_arr))
-    dut.in_2d_arr = 365
-    await Timer(1, "ns")
-    print("Getting: dut.out_2d_arr type %s" % type(dut.out_2d_arr))
-    if dut.out_2d_arr != 365:
-        raise TestFailure("Failed to readback dut.out_2d_arr")
+    assert dut.out_2d_arr.value == test_value
 
 
 @cocotb.test()
 async def test_in_vect_packed_packed_packed(dut):
+    test_value = 95869805
+    dut.in_vect_packed_packed_packed.value = test_value
     await Timer(1, "ns")
-    print("Setting: dut.in_vect_packed_packed_packed type %s" % type(dut.in_vect_packed_packed_packed))
-    dut.in_vect_packed_packed_packed = 95869805
-    await Timer(1, "ns")
-    print("Getting: dut.out_vect_packed_packed_packed type %s" % type(dut.out_vect_packed_packed_packed))
-    if dut.out_vect_packed_packed_packed != 95869805:
-        raise TestFailure("Failed to readback dut.out_vect_packed_packed_packed")
+    assert dut.out_vect_packed_packed_packed.value == test_value
 
 
-# Questa unable to access elements of a logic array if the last dimension is unpacked (gh-2605)
+# Questa is unable to access elements of a logic array if the last dimension is unpacked (gh-2605)
 @cocotb.test(
     expect_error=IndexError
     if cocotb.LANGUAGE == "verilog" and cocotb.SIM_NAME.lower().startswith("modelsim")
     else ()
 )
 async def test_in_vect_packed_packed_unpacked(dut):
+    test_value = [365, 365, 365]
+    dut.in_vect_packed_packed_unpacked.value = test_value
     await Timer(1, "ns")
-    print("Setting: dut.in_vect_packed_packed_unpacked type %s" % type(dut.in_vect_packed_packed_unpacked))
-    dut.in_vect_packed_packed_unpacked = [365, 365, 365]
-    await Timer(1, "ns")
-    print("Getting: dut.out_vect_packed_packed_unpacked type %s" % type(dut.out_vect_packed_packed_unpacked))
-    if dut.out_vect_packed_packed_unpacked != [365, 365, 365]:
-        raise TestFailure("Failed to readback dut.out_vect_packed_packed_unpacked")
+    assert dut.out_vect_packed_packed_unpacked.value == test_value
 
 
 @cocotb.test()
 async def test_in_vect_packed_unpacked_unpacked(dut):
+    test_value = 3 *[3 * [5] ]
+    dut.in_vect_packed_unpacked_unpacked.value = test_value
     await Timer(1, "ns")
-    print("Setting: dut.in_vect_packed_unpacked_unpacked type %s" % type(dut.in_vect_packed_unpacked_unpacked))
-    dut.in_vect_packed_unpacked_unpacked = 3 *[3 * [5] ]
-    await Timer(1, "ns")
-    print("Getting: dut.out_vect_packed_unpacked_unpacked type %s" % type(dut.out_vect_packed_unpacked_unpacked))
-    if dut.out_vect_packed_unpacked_unpacked != 3 *[3 * [5] ]:
-        raise TestFailure("Failed to readback dut.out_vect_packed_unpacked_unpacked")
+    assert dut.out_vect_packed_unpacked_unpacked.value == test_value
 
 
 @cocotb.test()
 async def test_in_vect_unpacked_unpacked_unpacked(dut):
+    test_value = 3 *[3 * [[1, 0, 1]]]
+    dut.in_vect_unpacked_unpacked_unpacked.value = test_value
     await Timer(1, "ns")
-    print("Setting: dut.in_vect_unpacked_unpacked_unpacked type %s" % type(dut.in_vect_unpacked_unpacked_unpacked))
-    dut.in_vect_unpacked_unpacked_unpacked = 3 *[3 * [[1, 0, 1]]]
-    await Timer(1, "ns")
-    print("Getting: dut.out_vect_unpacked_unpacked_unpacked type %s" % type(dut.out_vect_unpacked_unpacked_unpacked))
-    if dut.out_vect_unpacked_unpacked_unpacked != 3 *[3 * [[1, 0, 1]]]:
-        raise TestFailure("Failed to readback dut.out_vect_unpacked_unpacked_unpacked")
+    assert dut.out_vect_unpacked_unpacked_unpacked.value == test_value
 
 
 @cocotb.test()
 async def test_in_arr_packed_packed(dut):
+    test_value = (365 << 18) | (365 << 9) | (365)
+    dut.in_arr_packed_packed.value = test_value
     await Timer(1, "ns")
-    print("Setting: dut.in_arr_packed_packed type %s" % type(dut.in_arr_packed_packed))
-    dut.in_arr_packed_packed = (365 << 18) | (365 << 9) | (365)
-    await Timer(1, "ns")
-    print("Getting: dut.out_arr_packed_packed type %s" % type(dut.out_arr_packed_packed))
-    if dut.out_arr_packed_packed != (365 << 18) | (365 << 9) | (365):
-        raise TestFailure("Failed to readback dut.out_arr_packed_packed")
+    assert dut.out_arr_packed_packed.value == test_value
 
 
-# Questa unable to access elements of a logic array if the last dimension is unpacked (gh-2605)
+# Questa is unable to access elements of a logic array if the last dimension is unpacked (gh-2605)
 @cocotb.test(
     expect_error=IndexError
     if cocotb.LANGUAGE == "verilog" and cocotb.SIM_NAME.lower().startswith("modelsim")
     else ()
 )
 async def test_in_arr_packed_unpacked(dut):
+    test_value = [365, 365, 365]
+    dut.in_arr_packed_unpacked.value = test_value
     await Timer(1, "ns")
-    print("Setting: dut.in_arr_packed_unpacked type %s" % type(dut.in_arr_packed_unpacked))
-    dut.in_arr_packed_unpacked = [365, 365, 365]
-    await Timer(1, "ns")
-    print("Getting: dut.out_arr_packed_unpacked type %s" % type(dut.out_arr_packed_unpacked))
-    if dut.out_arr_packed_unpacked != [365, 365, 365]:
-        raise TestFailure("Failed to readback dut.out_arr_packed_unpacked")
+    assert dut.out_arr_packed_unpacked.value == test_value
 
 
 @cocotb.test()
 async def test_in_arr_unpacked_unpacked(dut):
+    test_value = 3 *[3 * [5] ]
+    dut.in_arr_unpacked_unpacked.value = test_value
     await Timer(1, "ns")
-    print("Setting: dut.in_arr_unpacked_unpacked type %s" % type(dut.in_arr_unpacked_unpacked))
-    dut.in_arr_unpacked_unpacked = 3 *[3 * [5] ]
-    await Timer(1, "ns")
-    print("Getting: dut.out_arr_unpacked_unpacked type %s" % type(dut.out_arr_unpacked_unpacked))
-    if dut.out_arr_unpacked_unpacked != 3 *[3 * [5] ]:
-        raise TestFailure("Failed to readback dut.out_arr_unpacked_unpacked")
+    assert dut.out_arr_unpacked_unpacked.value == test_value
 
 
 @cocotb.test()
 async def test_in_2d_arr_packed(dut):
+    test_value = (365 << 18) | (365 << 9) | (365)
+    dut.in_2d_arr_packed.value = test_value
     await Timer(1, "ns")
-    print("Setting: dut.in_2d_arr_packed type %s" % type(dut.in_2d_arr_packed))
-    dut.in_2d_arr_packed = (365 << 18) | (365 << 9) | (365)
-    await Timer(1, "ns")
-    print("Getting: dut.out_2d_arr_packed type %s" % type(dut.out_2d_arr_packed))
-    if dut.out_2d_arr_packed != (365 << 18) | (365 << 9) | (365):
-        raise TestFailure("Failed to readback dut.out_2d_arr_packed")
+    assert dut.out_2d_arr_packed.value == test_value
 
 
-# Questa unable to access elements of a logic array if the last dimension is unpacked (gh-2605)
+# Questa is unable to access elements of a logic array if the last dimension is unpacked (gh-2605)
 @cocotb.test(
     expect_error=IndexError
     if cocotb.LANGUAGE == "verilog" and cocotb.SIM_NAME.lower().startswith("modelsim")
     else ()
 )
 async def test_in_2d_arr_unpacked(dut):
+    test_value = [365, 365, 365]
+    dut.in_2d_arr_unpacked.value = test_value
     await Timer(1, "ns")
-    print("Setting: dut.in_2d_arr_unpacked type %s" % type(dut.in_2d_arr_unpacked))
-    dut.in_2d_arr_unpacked = [365, 365, 365]
-    await Timer(1, "ns")
-    print("Getting: dut.out_2d_arr_unpacked type %s" % type(dut.out_2d_arr_unpacked))
-    if dut.out_2d_arr_unpacked != [365, 365, 365]:
-        raise TestFailure("Failed to readback dut.out_2d_arr_unpacked")
+    assert dut.out_2d_arr_unpacked.value == test_value
 
 
 @cocotb.test()
 async def test_in_3d_arr(dut):
+    test_value = (365 << 18) | (365 << 9) | (365)
+    dut.in_3d_arr.value = test_value
     await Timer(1, "ns")
-    print("Setting: dut.in_3d_arr type %s" % type(dut.in_3d_arr))
-    dut.in_3d_arr = (365 << 18) | (365 << 9) | (365)
-    await Timer(1, "ns")
-    print("Getting: dut.out_3d_arr type %s" % type(dut.out_3d_arr))
-    if dut.out_3d_arr != (365 << 18) | (365 << 9) | (365):
-        raise TestFailure("Failed to readback dut.out_3d_arr")
+    assert dut.out_3d_arr.value == test_value
