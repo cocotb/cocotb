1ad7b59579540a2acd6028aeab7396cde319ca19
Kaleb Barrett <dev.ktbarrett@gmail.com>
2021-09-23 10:02:48 -0500

    Add license header

--- cocotb/_vendor/distutils_version.py
+++ cocotb/_vendor/distutils_version.py
@@ -1,3 +1,10 @@
+# Copyright (c) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
+# 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021 Python Software Foundation;
+# All Rights Reserved
+# Copyright cocotb contributors
+# Licensed under the Revised BSD License, see LICENSE for details.
+# SPDX-License-Identifier: BSD-3-Clause
+
 #
 # distutils/version.py
 #
