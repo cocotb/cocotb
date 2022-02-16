bde1db5c8d298a0540ac7e05401479b24e420a30
Philipp Wagner <mail@philipp-wagner.com>
2022-02-16 23:42:08 +0100

    Package metadata: declare support for Python 3.10

--- setup.py
+++ setup.py
@@ -121,6 +121,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: BSD License",
         "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
         "Framework :: cocotb",
