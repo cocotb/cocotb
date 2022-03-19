8271905d923487aaa50732900aee02fd6f1fe20a
maleiter <32867198+maleiter@users.noreply.github.com>
2022-03-19 16:50:13 +0100

    Add VHDL toplevel and a functional testcase in examples/mixed_language (#2912)

--- examples/mixed_language/hdl/toplevel.vhdl
+++ examples/mixed_language/hdl/toplevel.vhdl
@@ -1,7 +1,7 @@
 -- Example using mixed-language simulation
 --
 -- Here we have a VHDL toplevel that instantiates both SV and VHDL
---  sub entities
+-- sub entities
 library ieee;
 
 use ieee.std_logic_1164.all;
@@ -38,9 +38,54 @@ entity endian_swapper_mixed is
     );
 end;
 
-architecture impl of endian_swapper_mixed is begin
+architecture impl of endian_swapper_mixed is
 
-i_swapper_sv : entity work.endian_swapper_sv
+    -- The SV entity is instantiated as a component because cocotb is
+    -- executing vcom before vlog. Hence the toplevel VHDL file is compiled
+    -- before the SV module. Therefore, an entity instantiation is not possible here.
+    component endian_swapper_sv
+        generic (
+            DATA_BYTES              : integer := 8);
+        port (
+            clk                     : in    std_ulogic;
+            reset_n                 : in    std_ulogic;
+
+            stream_in_data          : in    std_ulogic_vector(DATA_BYTES*8-1 downto 0);
+            stream_in_empty         : in    std_ulogic_vector(2 downto 0);
+            stream_in_valid         : in    std_ulogic;
+            stream_in_startofpacket : in    std_ulogic;
+            stream_in_endofpacket   : in    std_ulogic;
+            stream_in_ready         : out   std_ulogic;
+
+            stream_out_data         : out   std_ulogic_vector(DATA_BYTES*8-1 downto 0);
+            stream_out_empty        : out   std_ulogic_vector(2 downto 0);
+            stream_out_valid        : out   std_ulogic;
+            stream_out_startofpacket: out   std_ulogic;
+            stream_out_endofpacket  : out   std_ulogic;
+            stream_out_ready        : in    std_ulogic;
+
+            csr_address             : in    std_ulogic_vector(1 downto 0);
+            csr_readdata            : out   std_ulogic_vector(31 downto 0);
+            csr_readdatavalid       : out   std_ulogic;
+            csr_read                : in    std_ulogic;
+            csr_write               : in    std_ulogic;
+            csr_waitrequest         : out   std_ulogic;
+            csr_writedata           : in    std_ulogic_vector(31 downto 0)
+        );
+  end component;
+
+
+    signal sv_to_vhdl_data:             std_ulogic_vector(DATA_BYTES*8-1 downto 0) ;
+    signal sv_to_vhdl_empty:            std_ulogic_vector(2 downto 0);
+    signal sv_to_vhdl_valid:            std_ulogic;
+    signal sv_to_vhdl_startofpacket:    std_ulogic;
+    signal sv_to_vhdl_endofpacket:      std_ulogic;
+    signal sv_to_vhdl_ready:            std_ulogic;
+
+    signal csr_waitrequest_vhdl, csr_waitrequest_sv : std_ulogic;
+
+begin
+i_swapper_sv : endian_swapper_sv
     generic map (
         DATA_BYTES              =>      DATA_BYTES
     ) port map (
@@ -54,20 +99,54 @@ i_swapper_sv : entity work.endian_swapper_sv
         stream_in_valid         =>      stream_in_valid,
         stream_in_ready         =>      stream_in_ready,
 
-        stream_out_empty        =>      stream_out_empty,
-        stream_out_data         =>      stream_out_data,
-        stream_out_endofpacket  =>      stream_out_endofpacket,
-        stream_out_startofpacket=>      stream_out_startofpacket,
-        stream_out_valid        =>      stream_out_valid,
-        stream_out_ready        =>      stream_out_ready,
+        stream_out_empty        =>      sv_to_vhdl_empty,
+        stream_out_data         =>      sv_to_vhdl_data,
+        stream_out_endofpacket  =>      sv_to_vhdl_endofpacket,
+        stream_out_startofpacket=>      sv_to_vhdl_startofpacket,
+        stream_out_valid        =>      sv_to_vhdl_valid,
+        stream_out_ready        =>      sv_to_vhdl_ready,
 
         csr_address             =>      csr_address,
         csr_readdata            =>      csr_readdata,
         csr_readdatavalid       =>      csr_readdatavalid,
         csr_read                =>      csr_read,
         csr_write               =>      csr_write,
-        csr_waitrequest         =>      csr_waitrequest,
+        csr_waitrequest         =>      csr_waitrequest_sv,
         csr_writedata           =>      csr_writedata
     );
 
+
+
+i_swapper_vhdl : entity work.endian_swapper_vhdl
+generic map (
+    DATA_BYTES              =>      DATA_BYTES
+) port map (
+    clk                     =>      clk,
+    reset_n                 =>      reset_n,
+
+    stream_in_empty         =>      sv_to_vhdl_empty,
+    stream_in_data          =>      sv_to_vhdl_data,
+    stream_in_endofpacket   =>      sv_to_vhdl_endofpacket,
+    stream_in_startofpacket =>      sv_to_vhdl_startofpacket,
+    stream_in_valid         =>      sv_to_vhdl_valid,
+    stream_in_ready         =>      sv_to_vhdl_ready,
+
+    stream_out_empty        =>      stream_out_empty,
+    stream_out_data         =>      stream_out_data,
+    stream_out_endofpacket  =>      stream_out_endofpacket,
+    stream_out_startofpacket=>      stream_out_startofpacket,
+    stream_out_valid        =>      stream_out_valid,
+    stream_out_ready        =>      stream_out_ready,
+
+    csr_address             =>      csr_address,
+    csr_readdata            =>      open,
+    csr_readdatavalid       =>      open,
+    csr_read                =>      csr_read,
+    csr_write               =>      csr_write,
+    csr_waitrequest         =>      csr_waitrequest_vhdl,
+    csr_writedata           =>      csr_writedata
+);
+
+
+    csr_waitrequest <= csr_waitrequest_sv or csr_waitrequest_vhdl;
 end architecture;
--- examples/mixed_language/tests/test_mixed_language.py
+++ examples/mixed_language/tests/test_mixed_language.py
@@ -1,14 +1,30 @@
+# Copyright cocotb contributors
+# Licensed under the Revised BSD License, see LICENSE for details.
+# SPDX-License-Identifier: BSD-3-Clause
+
 import cocotb
-from cocotb.triggers import Timer
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, Timer
 
 
-@cocotb.test()
-async def mixed_language_test(dut):
+# Riviera fails to find dut.i_swapper_sv (gh-2921)
+@cocotb.test(
+    expect_error=AttributeError
+    if cocotb.SIM_NAME.lower().startswith(("riviera", "aldec"))
+    and cocotb.LANGUAGE == "vhdl"
+    else ()
+)
+async def mixed_language_accessing_test(dut):
     """Try accessing handles and setting values in a mixed language environment."""
     await Timer(100, units="ns")
 
     verilog = dut.i_swapper_sv
     dut._log.info("Got: %s" % repr(verilog._name))
+
+    # discover all attributes of the SV component
+    # This is a workaround since SV modules are not discovered automatically
+    # when using Modelsim/Questa and a VHDL toplevel file.
+    verilog._discover_all()
 
     vhdl = dut.i_swapper_vhdl
     dut._log.info("Got: %s" % repr(vhdl._name))
@@ -24,3 +40,68 @@ async def mixed_language_test(dut):
     # Try accessing an object other than a port...
     verilog.flush_pipe.value
     vhdl.flush_pipe.value
+
+
+# Riviera fails to find dut.i_swapper_sv (gh-2921)
+@cocotb.test(
+    expect_error=AttributeError
+    if cocotb.SIM_NAME.lower().startswith(("riviera", "aldec"))
+    and cocotb.LANGUAGE == "vhdl"
+    else ()
+)
+async def mixed_language_functional_test(dut):
+    """Try concurrent simulation of VHDL and Verilog and check the output."""
+    await Timer(100, units="ns")
+
+    verilog = dut.i_swapper_sv
+    dut._log.info("Got: %s" % repr(verilog._name))
+
+    vhdl = dut.i_swapper_vhdl
+    dut._log.info("Got: %s" % repr(vhdl._name))
+
+    # setup default values
+    dut.reset_n.value = 0
+    dut.stream_out_ready.value = 1
+
+    dut.stream_in_startofpacket.value = 0
+    dut.stream_in_endofpacket.value = 0
+    dut.stream_in_data.value = 0
+    dut.stream_in_valid.value = 1
+    dut.stream_in_empty.value = 0
+
+    dut.csr_address.value = 0
+    dut.csr_read.value = 0
+    dut.csr_write.value = 0
+    dut.csr_writedata.value = 0
+
+    # reset cycle
+    await Timer(100, units="ns")
+    dut.reset_n.value = 1
+    await Timer(100, units="ns")
+
+    # start clock
+    cocotb.start_soon(Clock(dut.clk, 10, units="ns").start())
+    await Timer(500, units="ns")
+
+    previous_indata = 0
+    # transmit some packets
+    for _ in range(1, 5):
+
+        for i in range(1, 11):
+            await RisingEdge(dut.clk)
+            previous_indata = dut.stream_in_data.value
+
+            # write stream in data
+            dut.stream_in_data.value = i + 0x81FFFFFF2B00  # generate a magic number
+            dut.stream_in_valid.value = 1
+            await RisingEdge(dut.clk)
+            dut.stream_in_valid.value = 0
+
+            # await stream out data
+            await RisingEdge(dut.clk)
+            await RisingEdge(dut.clk)
+
+            # compare in and out data
+            assert int(previous_indata) == int(
+                dut.stream_out_data.value
+            ), f"stream in data and stream out data were different in round {i}"
