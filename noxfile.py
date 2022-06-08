b1ceed2b867af643b6af0563ecaaad3dd26d3999
Tomasz Hemperek <hemperek@physik.uni-bonn.de>
2022-06-08 21:52:09 +0200

    Add HDL_TOPLEVEL_LANG in nox

--- noxfile.py
+++ noxfile.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 import glob
 import shutil
+import os
 import sys
 from contextlib import suppress
 from pathlib import Path
@@ -74,9 +75,14 @@ def env_vars_for_test(
     e = {}
     if sim is not None:
         e["SIM"] = sim
+
+    if os.getenv("TOPLEVEL_LANG") is not None:
+        e["HDL_TOPLEVEL_LANG"] = os.getenv("TOPLEVEL_LANG")
+
     if toplevel_lang is not None:
         e["TOPLEVEL_LANG"] = toplevel_lang
         e["HDL_TOPLEVEL_LANG"] = toplevel_lang
+
     assert not (toplevel_lang == "verilog" and gpi_interface != "vpi")
     if toplevel_lang == "vhdl" and gpi_interface is not None:
         e["VHDL_GPI_INTERFACE"] = gpi_interface
