44f89135047e821c55f0ccb00a17b400c874fb61
Benjamin Menkuec <benjamin@menkuec.de>
2023-01-02 21:22:06 +0100

    add test for long parameters

---
+++ tests/test_cases/test_long_parameter/test_long_parameter.py
@@ -0,0 +1,11 @@
+# Copyright cocotb contributors
+# Licensed under the Revised BSD License, see LICENSE for details.
+# SPDX-License-Identifier: BSD-3-Clause
+import random
+
+import cocotb
+
+
+@cocotb.test()
+async def test_long_paramter(dut):
+    assert dut.INT_PARAM.value == 10**100
