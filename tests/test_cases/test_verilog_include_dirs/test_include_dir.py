df6539b89d147f507378606e1b819c6b34246983
Dave Keeshan <dave.keeshan@daxzio.com>
2023-01-24 20:23:23 +0000

    Rewrote testing to be a simple as is required to compile the compile and elaboration phase just before simulation, if it gets this far it is a pass, if not a fail

--- tests/test_cases/test_verilog_include_dirs/test_include_dir.py
+++ tests/test_cases/test_verilog_include_dirs/test_include_dir.py
@@ -1,6 +1,8 @@
 import cocotb
 
 
+# The purpose of this test is just to complete an elobation cycle up to time 0, before simulation
+# If it fails to get to this point then the addition of the include dirs failed!
 @cocotb.test()
 async def test_noop(_):
     pass
