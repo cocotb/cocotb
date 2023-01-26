e2557e213a65eb5a276a01f71b3bd3196afc7cc4
Dave Keeshan <96727608+davekeeshan@users.noreply.github.com>
2023-01-26 09:46:54 +0000

    Update tests/test_cases/test_verilog_include_dirs/test_include_dir.py
    Co-authored-by: Philipp Wagner <mail@philipp-wagner.com>

--- tests/test_cases/test_verilog_include_dirs/test_include_dir.py
+++ tests/test_cases/test_verilog_include_dirs/test_include_dir.py
@@ -1,7 +1,7 @@
 import cocotb
 
 
-# The purpose of this test is just to complete an elobation cycle up to time 0, before simulation
+# The purpose of this test is just to complete an elaboration cycle up to time 0, before simulation
 # If it fails to get to this point then the addition of the include dirs failed!
 @cocotb.test()
 async def test_noop(_):
