cocotb/cocotb

commit 249ebe0c145cdec46781c6ae379d406e0de30287

Add test cases for uncovered test failure modes

tests/test_cases/test_failure/Makefile
@@ -9,6 +9,6 @@ MODULE := test_failure
 override_for_this_test:
 	-$(MAKE) all
 	$(call check_for_results_file)
-	test $$(../../../bin/combine_results.py | grep "Failure in testsuite" | wc -l) -eq 1 && rm -f results.xml
+	test $$(../../../bin/combine_results.py | grep "Failure in testsuite" | wc -l) -eq 4 && rm -f results.xml
 
 include ../../designs/sample_module/Makefile

tests/test_cases/test_failure/test_failure.py
@@ -13,3 +13,18 @@ import cocotb
 @cocotb.test()
 async def test_fail(_):
     assert False
+
+
+@cocotb.test(expect_fail=True)
+async def test_pass_expect_fail(_):
+    assert True
+
+
+@cocotb.test(expect_error=Exception)
+async def test_pass_expect_error(_):
+    assert True
+
+
+@cocotb.test(expect_error=ValueError)
+async def test_wrong_error(_):
+    raise TypeError

commit a3c3d3e947a950f0bd78e3d9e636033ff5fa4542

Add support for pytest.raises "Failed" exception
Also reverts a change in behavior introduced in #3227.

cocotb/decorators.py
@@ -503,7 +503,9 @@ class test(coroutine, metaclass=_decorator_helper):
                 Using ``None`` as the *timeout_unit* argument is deprecated, use ``'step'`` instead.
 
         expect_fail (bool, optional):
-            Don't mark the result as a failure if the test fails.
+            If ``True`` and the test fails a functional check via an ``assert`` statement, :class:`pytest.raises`,
+            :class:`pytest.warns`, or :class:`pytest.deprecated_call` the test is considered to have passed.
+            If ``True`` and the test passes successfully, the test is considered to have failed.
 
         expect_error (exception type or tuple of exception types, optional):
             Mark the result as a pass only if one of the exception types is raised in the test.
@@ -589,7 +591,7 @@ class test(coroutine, metaclass=_decorator_helper):
                 stacklevel=2,
             )
         if expect_error is True:
-            expect_error = (Exception,)
+            expect_error = (BaseException,)
         elif expect_error is False:
             expect_error = ()
         self.expect_error = expect_error

cocotb/regression.py
@@ -37,7 +37,7 @@ import sys
 import time
 import traceback
 from itertools import product
-from typing import Any, Iterable, Optional, Tuple
+from typing import Any, Iterable, Optional, Tuple, Type
 
 import cocotb
 import cocotb.ANSI as ANSI
@@ -77,6 +77,20 @@ def _my_import(name: str) -> Any:
 
 
 _logger = SimLog(__name__)
+
+_Failed: Type[BaseException]
+try:
+    import pytest
+except ModuleNotFoundError:
+    _Failed = AssertionError
+else:
+    try:
+        with pytest.raises(Exception):
+            pass
+    except BaseException as _raises_e:
+        _Failed = type(_raises_e)
+    else:
+        assert "pytest.raises doesn't raise an exception when it fails"
 
 
 class RegressionManager:
@@ -422,15 +436,15 @@ class RegressionManager:
             # whether we expected it or not, the simulation has failed unrecoverably
             sim_failed = True
 
+        elif isinstance(result, (AssertionError, _Failed)) and test.expect_fail:
+            self._log_test_passed(test, result, "failed as expected")
+
         elif test.expect_error:
             if isinstance(result, test.expect_error):
                 self._log_test_passed(test, result, "errored as expected")
             else:
                 self._log_test_failed(test, result, "errored with unexpected type ")
                 result_pass = False
-
-        elif test.expect_fail:
-            self._log_test_passed(test, result, "failed as expected")
 
         else:
             self._log_test_failed(test, result, None)

tests/test_cases/test_cocotb/test_tests.py
@@ -11,7 +11,6 @@ Tests of cocotb.test functionality
 from collections.abc import Coroutine
 
 import pytest
-from _pytest.outcomes import Failed
 from common import MyBaseException, MyException
 
 import cocotb
@@ -135,13 +134,13 @@ async def test_empty_docstring(dut) -> None:
     """"""
 
 
-@cocotb.test(expect_error=Failed)
+@cocotb.test(expect_fail=True)
 async def test_pytest_raises_fail(dut):
     with pytest.raises(AssertionError):
         assert True
 
 
-@cocotb.test(expect_error=Failed)
+@cocotb.test(expect_fail=True)
 async def test_pytest_warns_fail(dut):
     def test_func():
         pass
@@ -150,7 +149,7 @@ async def test_pytest_warns_fail(dut):
         test_func()
 
 
-@cocotb.test(expect_error=Failed)
+@cocotb.test(expect_fail=True)
 async def test_pytest_deprecated_call_fail(dut):
     def test_func():
         pass
@@ -159,7 +158,7 @@ async def test_pytest_deprecated_call_fail(dut):
         test_func()
 
 
-@cocotb.test(expect_error=Failed)
+@cocotb.test(expect_fail=True)
 async def test_pytest_raises_fail_in_task(dut):
     async def test_func():
         with pytest.raises(AssertionError):
@@ -169,7 +168,7 @@ async def test_pytest_raises_fail_in_task(dut):
     await NullTrigger()
 
 
-@cocotb.test(expect_error=Failed)
+@cocotb.test(expect_fail=True)
 async def test_pytest_warns_fail_in_task(dut):
     def inner_func():
         pass
@@ -182,7 +181,7 @@ async def test_pytest_warns_fail_in_task(dut):
     await NullTrigger()
 
 
-@cocotb.test(expect_error=Failed)
+@cocotb.test(expect_fail=True)
 async def test_pytest_deprecated_call_fail_in_task(dut):
     def inner_func():
         pass
@@ -195,35 +194,15 @@ async def test_pytest_deprecated_call_fail_in_task(dut):
     await NullTrigger()
 
 
-@cocotb.test(expect_fail=True)
-async def test_pytest_raises_fail_expect_fail(dut):
-    with pytest.raises(AssertionError):
-        assert True
-
-
-@cocotb.test(expect_fail=True)
+@cocotb.test(expect_error=MyBaseException)
 async def test_base_exception_expect_fail(dut):
     raise MyBaseException
 
 
-@cocotb.test(expect_fail=True)
+@cocotb.test(expect_error=MyBaseException)
 async def test_base_exception_in_task_expect_fail(dut):
     async def test_func():
         raise MyBaseException
-
-    cocotb.start_soon(test_func())
-    await NullTrigger()
-
-
-@cocotb.test(expect_fail=True)
-async def test_exception_expect_fail(dut):
-    raise MyException()
-
-
-@cocotb.test(expect_fail=True)
-async def test_exception_in_task_expect_fail(dut):
-    async def test_func():
-        raise MyException
 
     cocotb.start_soon(test_func())
     await NullTrigger()

tests/test_cases/test_failure/Makefile
@@ -9,6 +9,6 @@ MODULE := test_failure
 override_for_this_test:
 	-$(MAKE) all
 	$(call check_for_results_file)
-	test $$(../../../bin/combine_results.py | grep "Failure in testsuite" | wc -l) -eq 4 && rm -f results.xml
+	test $$(../../../bin/combine_results.py | grep "Failure in testsuite" | wc -l) -eq 5 && rm -f results.xml
 
 include ../../designs/sample_module/Makefile

tests/test_cases/test_failure/test_failure.py
@@ -28,3 +28,8 @@ async def test_pass_expect_error(_):
 @cocotb.test(expect_error=ValueError)
 async def test_wrong_error(_):
     raise TypeError
+
+
+@cocotb.test(expect_fail=True)
+async def test_expect_fail_but_errors(_):
+    raise Exception()