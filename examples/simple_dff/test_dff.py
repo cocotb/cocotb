189845cdfdb77369ab8cd75f4d920290bdc306bd
Dave Dribin <dave-vcs@dribin.org>
2023-05-09 09:55:50 -0500

    Update test_dff.py to work with VHDL via GHDL

--- examples/simple_dff/test_dff.py
+++ examples/simple_dff/test_dff.py
@@ -16,25 +16,27 @@ from cocotb.types import LogicArray
 async def dff_simple_test(dut):
     """Test that d propagates to q"""
 
-    # Set initial input value to prevent it from floating
-    dut.d.setimmediatevalue(1)
     # Assert initial output is unknown
     assert LogicArray(dut.q.value) == LogicArray("X")
+    # Set initial input value to prevent it from floating
+    dut.d.setimmediatevalue(0)
 
     clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
-    cocotb.start_soon(clock.start())  # Start the clock
+    # Start the clock. Start it low to avoid issues on the first RisingEdge
+    cocotb.start_soon(clock.start(start_high=False))
 
-    await RisingEdge(dut.clk)  # Synchronize with the clock
-    expected_val = 1  # Matches initial input value
+    # Synchronize with the clock. This will regisiter the initial `d` value
+    await RisingEdge(dut.clk)
+    expected_val = 0  # Matches initial input value
     for i in range(10):
         val = random.randint(0, 1)
         dut.d.value = val  # Assign the random value val to the input port d
         await RisingEdge(dut.clk)
         assert dut.q.value == expected_val, f"output q was incorrect on the {i}th cycle"
-        expected_val = val
+        expected_val = val # Save random value for next RisingEdge
 
     # Check the final input on the next clock
-    await RisingEdge(dut.clk)  # Synchronize with the clock
+    await RisingEdge(dut.clk)
     assert dut.q.value == expected_val, "output q was incorrect on the last cycle"
 
 
