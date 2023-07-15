42e521ce99f159fd945ff92369e41e73c7ec7229
Philipp Wagner <mail@philipp-wagner.com>
2023-07-15 16:49:46 +0200

    Xcelium: Skip mixed-language discovery tests
    The mixed-language discovery tests result in a nested VPI/VHPI call
    structure, similar to what's tracked in
    https://github.com/cocotb/cocotb/issues/1076. Cadence is working on a
    fix, disable these tests until #1076 is fixed and we can re-evaluate.
    
    (cherry picked from commit a18c513fd0f6b006376786ff136706a28470c058)

--- tests/test_cases/test_iteration_mixedlang/test_iteration.py
+++ tests/test_cases/test_iteration_mixedlang/test_iteration.py
@@ -68,7 +68,10 @@ async def test_loads(dut):
         tlog.info("Found %s" % repr(load))
 
 
-@cocotb.test()
+# Xcelium fails with "FATAL: We are calling up again".
+# TODO: Re-evaluate waiver once https://github.com/cocotb/cocotb/issues/1076 is
+# fixed.
+@cocotb.test(skip=cocotb.SIM_NAME.startswith("xmsim"))
 async def recursive_discovery(dut):
     """Recursively discover every single object in the design."""
     if cocotb.SIM_NAME.lower().startswith(("ncsim", "xmsim")):
@@ -93,7 +96,10 @@ async def recursive_discovery(dut):
     )
 
 
-@cocotb.test()
+# Xcelium fails with "FATAL: We are calling up again".
+# TODO: Re-evaluate waiver once https://github.com/cocotb/cocotb/issues/1076 is
+# fixed.
+@cocotb.test(skip=cocotb.SIM_NAME.startswith("xmsim"))
 async def recursive_discovery_boundary(dut):
     """Iteration through the boundary works but this just double checks."""
     if cocotb.SIM_NAME.lower().startswith(("ncsim", "xmsim")):
