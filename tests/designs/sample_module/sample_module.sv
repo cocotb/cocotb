0be7149de611a8dad1797c6df842acab86c0a5a3
Robert Winkler <rwinkler@antmicro.com>
2023-08-08 12:52:31 +0200

    review: Provide tests for checking waveform generation
    Internal-tag: [#47208]
    Signed-off-by: Robert Winkler <rwinkler@antmicro.com>

--- tests/designs/sample_module/sample_module.sv
+++ tests/designs/sample_module/sample_module.sv
@@ -119,10 +119,12 @@ test_if struct_var;
 
 and test_and_gate(and_output, stream_in_ready, stream_in_valid);
 
+`ifndef NODUMPFILE
 initial begin
     $dumpfile("waveform.vcd");
     $dumpvars(0,sample_module);
 end
+`endif
 
 parameter NUM_OF_MODULES = 4;
 reg[NUM_OF_MODULES-1:0] temp;
