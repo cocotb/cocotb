8c964a8ced3cb7bed3fa041084548d67fff7ec36
Philipp Wagner <mail@philipp-wagner.com>
2023-10-10 22:25:18 +0200

    Run tests without clean from nox
    In nox, run "make test" without a "make clean" run first. "make clean" for
    the full test suite takes multiple minutes (mostly due to the overhead
    of repeatedly calling cocotb-config) and is unnecessary on CI runners,
    which always start from a clean state. Remove "make clean" from the
    noxfile, developers can still call it on demand if necessary.

--- noxfile.py
+++ noxfile.py
@@ -165,7 +165,7 @@ def dev_test_sim(
         coverage_file.unlink()
 
     session.log(f"Running 'make test' against a simulator {config_str}")
-    session.run("make", "clean", "test", external=True, env=env)
+    session.run("make", "test", external=True, env=env)
 
     session.log(f"Running simulator-specific tests against a simulator {config_str}")
     session.run(
@@ -479,7 +479,7 @@ def release_test_sim(
     config_str = stringify_dict(env)
 
     session.log(f"Running tests against a simulator: {config_str}")
-    session.run("make", "clean", "test", external=True, env=env)
+    session.run("make", "test", external=True, env=env)
 
     session.log(f"Running simulator-specific tests against a simulator {config_str}")
     session.run(
