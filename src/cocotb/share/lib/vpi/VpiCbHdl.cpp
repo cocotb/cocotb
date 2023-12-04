c11d1e9a123720edb554c4a1af52eaa3abc7a00f
Todd Strader <tstrader@hudson-trading.com>
2023-12-04 16:55:16 -0500

    lint

--- src/cocotb/share/lib/vpi/VpiCbHdl.cpp
+++ src/cocotb/share/lib/vpi/VpiCbHdl.cpp
@@ -766,11 +766,12 @@ GpiIterator::Status VpiPackageIterator::next_handle(std::string &,
     VpiImpl *vpi_impl = reinterpret_cast<VpiImpl *>(m_impl);
     std::string name = vpi_get_str(vpiName, obj);
     std::string fq_name = vpi_get_str(vpiFullName, obj);
-    LOG_DEBUG("VPI: package iterator found '%s' = '%s'", name.c_str(), fq_name.c_str());
+    LOG_DEBUG("VPI: package iterator found '%s' = '%s'", name.c_str(),
+              fq_name.c_str());
     // '::' may or may not be included in package names:
     std::string package_delim = "::";
     if (fq_name.compare(fq_name.length() - package_delim.length(),
-                           package_delim.length(), package_delim)) {
+                        package_delim.length(), package_delim)) {
         fq_name += "::";
     }
     new_obj = new GpiObjHdl(vpi_impl, obj, GPI_PACKAGE);
