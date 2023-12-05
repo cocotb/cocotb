fbf1e56a00acf1f6f7c024072e1a887fb4ed3bc1
Todd Strader <tstrader@hudson-trading.com>
2023-12-05 08:19:52 -0500

    nocommits

--- tests/designs/package_module/cocotb_package.sv
+++ tests/designs/package_module/cocotb_package.sv
@@ -8,20 +8,4 @@ module cocotb_package;
     import cocotb_package_pkg_2::*;
 
     parameter int seven_int = 7;
-`ifndef __ICARUS__
-    // NOCOMMIT -- can't properly access these parameters
-    // is this known?  open separate issue?
-    parameter logic [31:0] [3:0] packed_4567 = {
-        32'd4,
-        32'd5,
-        32'd6,
-        32'd7
-    };
-    parameter logic [31:0] unpacked_9876 [3:0] = {
-        32'd9,
-        32'd8,
-        32'd7,
-        32'd6
-    };
-`endif
 endmodule
