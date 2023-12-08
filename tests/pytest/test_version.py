07dd4be8eb62db2b1232cce6095267de303ffa23
Todd Strader <tstrader@hudson-trading.com>
2023-12-08 13:22:32 -0500

    test

---
+++ tests/pytest/test_version.py
@@ -0,0 +1,16 @@
+# Copyright cocotb contributors
+# Licensed under the Revised BSD License, see LICENSE for details.
+# SPDX-License-Identifier: BSD-3-Clause
+import pytest
+import cocotb
+import os
+import shutil
+import subprocess
+
+
+def test_version():
+    if "dev" in cocotb.__version__ and os.path.exists(".git") and shutil.which("git"):
+        assert "+" in cocotb.__version__
+        parts = cocotb.__version__.split("+")
+        rev = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"], text=True).strip()
+        assert parts[1] == rev
