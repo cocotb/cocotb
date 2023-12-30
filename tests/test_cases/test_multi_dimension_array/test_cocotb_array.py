f0457797a6c75cd1b381c060b462f1f0fa0163e2
Marlon James <marlon.james@gmail.com>
2023-12-29 18:54:19 -0800

    Verilator: Expect failures on some unpacked arrays (see #3611)
    (cherry picked from commit e94da9596f43b2cc924dcf7f4cb1c63c55419a0f)

--- tests/test_cases/test_multi_dimension_array/test_cocotb_array.py
+++ tests/test_cases/test_multi_dimension_array/test_cocotb_array.py
@@ -1,6 +1,8 @@
 import cocotb
 from cocotb.triggers import Timer
 
+SIM_NAME = cocotb.SIM_NAME.lower()
+
 
 @cocotb.test()
 async def test_in_vect_packed(dut):
@@ -10,7 +12,8 @@ async def test_in_vect_packed(dut):
     assert dut.out_vect_packed.value == test_value
 
 
-@cocotb.test()
+# Verilator combines 1-dimensional unpacked arrays into a single vector (gh-3611)
+@cocotb.test(expect_error=TypeError if SIM_NAME.startswith("verilator") else ())
 async def test_in_vect_unpacked(dut):
     test_value = [0x1, 0x0, 0x1]
     dut.in_vect_unpacked.value = test_value
@@ -42,7 +45,8 @@ async def test_in_2d_vect_packed_unpacked(dut):
     assert dut.out_2d_vect_packed_unpacked.value == test_value
 
 
-@cocotb.test()
+# Verilator doesn't support multi-dimensional unpacked arrays (gh-3611)
+@cocotb.test(expect_error=AttributeError if SIM_NAME.startswith("verilator") else ())
 async def test_in_2d_vect_unpacked_unpacked(dut):
     test_value = 3 * [[0x1, 0x0, 0x1]]
     dut.in_2d_vect_unpacked_unpacked.value = test_value
@@ -83,9 +87,12 @@ async def test_in_vect_packed_packed_packed(dut):
 
 
 # Questa is unable to access elements of a logic array if the last dimension is unpacked (gh-2605)
+# Verilator doesn't support multi-dimensional unpacked arrays (gh-3611)
 @cocotb.test(
     expect_error=IndexError
-    if cocotb.LANGUAGE == "verilog" and cocotb.SIM_NAME.lower().startswith("modelsim")
+    if cocotb.LANGUAGE == "verilog" and SIM_NAME.startswith("modelsim")
+    else AttributeError
+    if SIM_NAME.startswith("verilator")
     else ()
 )
 async def test_in_vect_packed_packed_unpacked(dut):
@@ -95,7 +102,8 @@ async def test_in_vect_packed_packed_unpacked(dut):
     assert dut.out_vect_packed_packed_unpacked.value == test_value
 
 
-@cocotb.test()
+# Verilator doesn't support multi-dimensional unpacked arrays (gh-3611)
+@cocotb.test(expect_error=AttributeError if SIM_NAME.startswith("verilator") else ())
 async def test_in_vect_packed_unpacked_unpacked(dut):
     test_value = 3 * [3 * [5]]
     dut.in_vect_packed_unpacked_unpacked.value = test_value
@@ -103,7 +111,8 @@ async def test_in_vect_packed_unpacked_unpacked(dut):
     assert dut.out_vect_packed_unpacked_unpacked.value == test_value
 
 
-@cocotb.test()
+# Verilator doesn't support multi-dimensional unpacked arrays (gh-3611)
+@cocotb.test(expect_error=AttributeError if SIM_NAME.startswith("verilator") else ())
 async def test_in_vect_unpacked_unpacked_unpacked(dut):
     test_value = 3 * [3 * [[1, 0, 1]]]
     dut.in_vect_unpacked_unpacked_unpacked.value = test_value
@@ -120,9 +129,12 @@ async def test_in_arr_packed_packed(dut):
 
 
 # Questa is unable to access elements of a logic array if the last dimension is unpacked (gh-2605)
+# Verilator doesn't support multi-dimensional unpacked arrays (gh-3611)
 @cocotb.test(
     expect_error=IndexError
-    if cocotb.LANGUAGE == "verilog" and cocotb.SIM_NAME.lower().startswith("modelsim")
+    if cocotb.LANGUAGE == "verilog" and SIM_NAME.startswith("modelsim")
+    else AttributeError
+    if SIM_NAME.startswith("verilator")
     else ()
 )
 async def test_in_arr_packed_unpacked(dut):
@@ -132,7 +144,8 @@ async def test_in_arr_packed_unpacked(dut):
     assert dut.out_arr_packed_unpacked.value == test_value
 
 
-@cocotb.test()
+# Verilator doesn't support multi-dimensional unpacked arrays (gh-3611)
+@cocotb.test(expect_error=AttributeError if SIM_NAME.startswith("verilator") else ())
 async def test_in_arr_unpacked_unpacked(dut):
     test_value = 3 * [3 * [5]]
     dut.in_arr_unpacked_unpacked.value = test_value
@@ -149,9 +162,12 @@ async def test_in_2d_arr_packed(dut):
 
 
 # Questa is unable to access elements of a logic array if the last dimension is unpacked (gh-2605)
+# Verilator doesn't support multi-dimensional unpacked arrays (gh-3611)
 @cocotb.test(
     expect_error=IndexError
-    if cocotb.LANGUAGE == "verilog" and cocotb.SIM_NAME.lower().startswith("modelsim")
+    if cocotb.LANGUAGE == "verilog" and SIM_NAME.startswith("modelsim")
+    else AttributeError
+    if SIM_NAME.startswith("verilator")
     else ()
 )
 async def test_in_2d_arr_unpacked(dut):
