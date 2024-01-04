bf3e879c95d6aa9ed15ea93ad0eddee203f03779
Kaleb Barrett <dev.ktbarrett@gmail.com>
2024-01-04 09:52:38 -0600

    Add test decorator back
    Accidentally removed test decorator in previous commit.

--- tests/test_cases/test_iteration_mixedlang/test_iteration.py
+++ tests/test_cases/test_iteration_mixedlang/test_iteration.py
@@ -73,6 +73,7 @@ async def test_loads(dut):
         tlog.info("Found %s" % repr(load))
 
 
+@cocotb.test
 async def recursive_discovery(dut):
     """Recursively discover every single object in the design."""
     if cocotb.SIM_NAME.lower().startswith(("ncsim", "xmsim")):
@@ -97,6 +98,7 @@ async def recursive_discovery(dut):
     )
 
 
+@cocotb.test
 async def recursive_discovery_boundary(dut):
     """Iteration through the boundary works but this just double checks."""
     if cocotb.SIM_NAME.lower().startswith(("ncsim", "xmsim")):
