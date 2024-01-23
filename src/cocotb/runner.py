1a2d0ca230f203f5b619b9ef463060c8029d9755
Todd Strader <tstrader@hudson-trading.com>
2024-01-23 17:01:56 -0500

    Verilator runner test_args

--- src/cocotb/runner.py
+++ src/cocotb/runner.py
@@ -1055,7 +1055,12 @@ class Verilator(Simulator):
 
     def _test_command(self) -> List[Command]:
         out_file = self.build_dir / self.sim_hdl_toplevel
-        return [[str(out_file)] + (["--trace"] if self.waves else []) + self.plusargs]
+        return [
+            [str(out_file)]
+            + (["--trace"] if self.waves else [])
+            + self.test_args
+            + self.plusargs
+        ]
 
 
 class Xcelium(Simulator):
