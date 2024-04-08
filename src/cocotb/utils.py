aa9a5b45cc62f605945c614a38802b1bf534c74f
Kaleb Barrett <dev.ktbarrett@gmail.com>
2024-04-07 21:27:17 -0600

    Correct typing on Timer and get_sim_steps
    Python type checkers ignore the subtype relation between numbers.Real
    and float because Guido doesn't like numbers. So we specifically add it
    to the list.

--- src/cocotb/utils.py
+++ src/cocotb/utils.py
@@ -104,7 +104,7 @@ def get_time_from_sim_steps(steps: int, units: str) -> int:
 
 
 def get_sim_steps(
-    time: Union[Real, Decimal], units: str = "step", *, round_mode: str = "error"
+    time: Union[float, Real, Decimal], units: str = "step", *, round_mode: str = "error"
 ) -> int:
     """Calculates the number of simulation time steps for a given amount of *time*.
 
