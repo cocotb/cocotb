33f0187c8b40bc7c414e0d71f0876360008fcc67
Kaleb Barrett <dev.ktbarrett@gmail.com>
2024-06-11 23:33:51 -0600

    Remove cocotb.result.SimFailure
    Doing this led to a healthy refactor and simplification of some of the
    RegressionManager.

--- src/cocotb/__init__.py
+++ src/cocotb/__init__.py
@@ -266,16 +266,12 @@ def _stop_library_coverage() -> None:
         _library_coverage.save()  # pragma: no cover
 
 
-def _sim_event(message: str) -> None:
+def _sim_event(msg: str) -> None:
     """Function that can be called externally to signal an event."""
-    from cocotb.result import SimFailure
-
     # We simply return here as the simulator will exit
     # so no cleanup is needed
-    msg = f"Failing test at simulator request before test run completion: {message}"
-    if _scheduler is not None:
-        _scheduler.log.error(msg)
-        _scheduler._finish_scheduler(SimFailure(msg))
+    if regression_manager is not None:
+        regression_manager._fail_simulation(msg)
     else:
         log.error(msg)
         _stop_user_coverage()
--- tests/test_cases/issue_1279/issue_1279.py
+++ tests/test_cases/issue_1279/issue_1279.py
@@ -1,5 +1,5 @@
 """
-Test that once a SimFailure occurs, no further tests are run
+Test that once a simulation failure occurs, no further tests are run
 """
 
 import cocotb
@@ -7,8 +7,7 @@ import cocotb
 
 @cocotb.test(
     skip=cocotb.SIM_NAME.lower().startswith("riviera"),  # gh-1859
-    expect_error=cocotb.result.SimFailure,
-    stage=1,
+    _expect_sim_failure=True,
 )
 async def test_sim_failure_a(dut):
     # invoke a deadlock, as nothing is driving this clock
@@ -17,8 +16,7 @@ async def test_sim_failure_a(dut):
 
 @cocotb.test(
     skip=cocotb.SIM_NAME.lower().startswith("riviera"),  # gh-1859
-    expect_error=cocotb.result.SimFailure,
-    stage=2,
+    _expect_sim_failure=True,
 )
 async def test_sim_failure_b(dut):
     assert False, "This test should never run"
