450d6f8a19b0ffb0ad3dba5d3a60ca9031d6bef7
Marlon James <marlon.james@gmail.com>
2024-07-11 23:25:23 -0700

    Fix docstring for cocotb.simulator.package_iterate()

--- src/cocotb/share/lib/simulator/simulatormodule.cpp
+++ src/cocotb/share/lib/simulator/simulatormodule.cpp
@@ -913,7 +913,7 @@ static PyMethodDef SimulatorMethods[] = {
      PyDoc_STR("package_iterate(/)\n"
                "--\n\n"
                "package_iterate() -> cocotb.simulator.gpi_iterator_hdl\n"
-               "Get an iterator handle to loop over all packages."
+               "Get an iterator handle to loop over all packages.\n"
                "\n"
                ".. versionadded:: 2.0")},
     {"register_timed_callback", register_timed_callback, METH_VARARGS,
