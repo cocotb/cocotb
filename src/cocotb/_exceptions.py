90dac7ede02affeb18aa65e44dc1bfc0eb48578e
Kaleb Barrett <dev.ktbarrett@gmail.com>
2024-07-25 23:03:18 -0600

    Move InternalError to global exception package

---
+++ src/cocotb/_exceptions.py
@@ -0,0 +1,7 @@
+# Copyright cocotb contributors
+# Licensed under the Revised BSD License, see LICENSE for details.
+# SPDX-License-Identifier: BSD-3-Clause
+
+
+class InternalError(BaseException):
+    """An error internal to scheduler. If you see this, report a bug!"""
