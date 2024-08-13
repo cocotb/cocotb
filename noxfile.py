a4df0d57ae0ab645533788951351d933d926060d
Kaleb Barrett <dev.ktbarrett@gmail.com>
2024-08-13 13:50:22 -0500

    Update build following change to setuptools/distutils (#4095)
    * Set CXXFLAGS appropriately now that they are being used
    * Update LDCXXSHARED variable now that it is being used
    * Add verbose switch when compiling cocotb for testing purposes
    
    (cherry picked from commit 1929837457788935c6c1f87cb3bc7c0108e442ff)

--- noxfile.py
+++ noxfile.py
@@ -113,14 +113,23 @@ def build_cocotb_for_dev_test(session: nox.Session, *, editable: bool) -> None:
     """
 
     env = session.env.copy()
-    env["CFLAGS"] = "-Werror -Wno-deprecated-declarations -g --coverage"
-    env["CXXFLAGS"] = "-Werror"
+    flags = " ".join(
+        [
+            "-Werror",
+            "-Wno-error=deprecated-declarations",
+            "-Wsuggest-override",
+            "-g",
+            "--coverage",
+        ]
+    )
+    env["CFLAGS"] = flags
+    env["CXXFLAGS"] = flags
     env["LDFLAGS"] = "--coverage"
 
     if editable:
-        session.run("pip", "install", "-e", ".", env=env)
+        session.run("pip", "install", "-v", "-e", ".", env=env)
     else:
-        session.run("pip", "install", ".", env=env)
+        session.run("pip", "install", "-v", ".", env=env)
 
 
 #
