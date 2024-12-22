4149e25113a65f01440daa68df7a531e8573a569
Kaleb Barrett <ktbarrett@hudson-trading.com>
2024-12-21 23:03:02 -0500

    Support multiple PYGPI_ENTRY_POINTs

--- src/pygpi/entry.py
+++ src/pygpi/entry.py
@@ -1,26 +1,36 @@
 import importlib
 import os
 from functools import reduce
-from types import ModuleType
-from typing import Callable, Tuple
+from typing import Any, Callable, List, Tuple, cast
 
 
-def load_entry() -> Tuple[ModuleType, Callable]:
+def load_entry(argv: List[str]) -> Any:
     """Gather entry point information by parsing :envvar:`PYGPI_ENTRY_POINT`."""
+
     entry_point_str = os.environ.get(
         "PYGPI_ENTRY_POINT", "cocotb:_initialise_testbench"
     )
+
+    # Parse the entry point string of the form "module:func,module:func,...".
+    # Any failure prevents any entry points from being loaded.
+    entry_points: List[Tuple[str, str]] = []
     try:
-        if ":" not in entry_point_str:
-            raise ValueError(
-                "Invalid PYGPI_ENTRY_POINT, missing entry function (no colon)."
-            )
-        entry_module_str, entry_func_str = entry_point_str.split(":", 1)
-        entry_module = importlib.import_module(entry_module_str)
-        entry_func = reduce(getattr, entry_func_str.split("."), entry_module)
+        entry_points_str = entry_point_str.split(",")
+        for entry_point_str in entry_points_str:
+            entry_module_str, entry_func_str = entry_point_str.split(":")
+            # TODO maybe some basic validation of the module and function names.
+            # WITHOUT IMPORTING THEM.
+            entry_points.append((entry_module_str, entry_func_str))
     except Exception as e:
         raise RuntimeError(
             f"Failure to parse PYGPI_ENTRY_POINT ('{entry_point_str}')"
         ) from e
-    else:
-        return entry_module, entry_func
+
+    # Run all entry points.
+    # Expect failure to stop the loading of any additional entry points.
+    for entry_module_str, entry_func_str in entry_points:
+        entry_module = importlib.import_module(entry_module_str)
+        entry_func: Callable[[List[str]], Any] = reduce(
+            getattr, entry_func_str.split("."), cast(Any, entry_module)
+        )
+        entry_func(argv)
