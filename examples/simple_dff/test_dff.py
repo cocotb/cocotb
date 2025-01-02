2eda2bd75ed30153225b98ed183f8131f22c8ad0
Tomasz Hemperek <themperek@users.noreply.github.com>
2025-01-02 18:48:51 +0100

    Correct LANGUAGE in simple_dff (#4329)

--- examples/simple_dff/test_dff.py
+++ examples/simple_dff/test_dff.py
@@ -11,7 +11,7 @@ from cocotb.triggers import RisingEdge
 from cocotb.types import LogicArray
 from cocotb_tools.runner import get_runner
 
-LANGUAGE = os.environ["TOPLEVEL_LANG"].lower().strip()
+LANGUAGE = os.getenv("HDL_TOPLEVEL_LANG", "verilog").lower().strip()
 
 
 @cocotb.test()
@@ -52,12 +52,11 @@ async def dff_simple_test(dut):
 
 
 def test_simple_dff_runner():
-    hdl_toplevel_lang = os.getenv("HDL_TOPLEVEL_LANG", "verilog")
     sim = os.getenv("SIM", "icarus")
 
     proj_path = Path(__file__).resolve().parent
 
-    if hdl_toplevel_lang == "verilog":
+    if LANGUAGE == "verilog":
         sources = [proj_path / "dff.sv"]
     else:
         sources = [proj_path / "dff.vhdl"]
