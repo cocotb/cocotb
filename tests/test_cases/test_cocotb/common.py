f8d19d2b0d39a4842933de5c0ac1eb7c728da2a8
Kaleb Barrett <dev.ktbarrett@gmail.com>
2025-02-18 15:34:35 -0500

    Create assert_takes contextmanager for unit tests

--- tests/test_cases/test_cocotb/common.py
+++ tests/test_cases/test_cocotb/common.py
@@ -5,8 +5,14 @@
 Common utilities shared by many tests in this directory
 """
 
+import operator
 import re
 import traceback
+from contextlib import contextmanager
+from typing import Callable, Generator
+
+from cocotb._typing import TimeUnit
+from cocotb.utils import get_sim_steps, get_sim_time, get_time_from_sim_steps
 
 
 async def _check_traceback(running_coro, exc_type, pattern, *match_args):
@@ -29,3 +35,29 @@ class MyException(Exception): ...
 
 
 class MyBaseException(BaseException): ...
+
+
+@contextmanager
+def assert_takes(
+    time: float, unit: TimeUnit, cmp: Callable[[int, int], bool] = operator.eq
+) -> Generator[None, None, None]:
+    """Assert that the block takes a certain amount of time to finish.
+
+    The *cmp* function passes actual after first argument and expected as the second.
+    Other useful comparison functions are: :func:`math.isclose`, and the functions in the
+    :mod:`operator` module.
+
+    Args:
+        time: Time value.
+        unit: Unit of *time*.
+        cmp: Comparison function to use. Defaults to equality.
+    """
+    expected = get_sim_steps(time, unit)
+    start = get_sim_time("step")
+    yield
+    end = get_sim_time("step")
+    actual = end - start
+    actual_in_units = get_time_from_sim_steps(actual, unit)
+    assert cmp(actual, expected), (
+        f"Expected the code to take {time}{unit}, actually took {actual_in_units}{unit}"
+    )
