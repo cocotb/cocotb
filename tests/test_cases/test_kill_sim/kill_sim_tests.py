b52233e884113fc9485fbd34dca91b813a5c0d97
Kaleb Barrett <dev.ktbarrett@gmail.com>
2025-04-11 08:22:40 -0600

    Add tests for killing sim after trigger

--- tests/test_cases/test_kill_sim/kill_sim_tests.py
+++ tests/test_cases/test_kill_sim/kill_sim_tests.py
@@ -5,6 +5,7 @@ import sys
 from typing import Any
 
 import cocotb
+from cocotb.triggers import Timer
 
 
 def make_failure_file() -> None:
@@ -36,6 +37,18 @@ async def test_task_sys_exit_sim_continued(_: Any) -> None:
     make_failure_file()
 
 
+@cocotb.test(expect_error=SystemExit)
+async def test_trigger_sys_exit(_: Any) -> None:
+    await Timer(1)
+    sys.exit(1)
+    make_failure_file()
+
+
+@cocotb.test(_expect_sim_failure=True)
+async def test_trigger_sys_exit_sim_continued(_: Any) -> None:
+    make_failure_file()
+
+
 @cocotb.test(expect_error=KeyboardInterrupt)
 async def test_keyboard_interrupt(_: Any) -> None:
     raise KeyboardInterrupt  # Analogous to Ctrl-C
@@ -58,4 +71,16 @@ async def test_task_keyboard_interrupt(_: Any) -> None:
 
 @cocotb.test(_expect_sim_failure=True)
 async def test_task_keyboard_interrupt_sim_continued(_: Any) -> None:
+    make_failure_file()
+
+
+@cocotb.test(expect_error=KeyboardInterrupt)
+async def test_trigger_keyboard_interrupt(_: Any) -> None:
+    await Timer(1)
+    raise KeyboardInterrupt  # Analogous to Ctrl-C
+    make_failure_file()
+
+
+@cocotb.test(_expect_sim_failure=True)
+async def test_trigger_keyboard_interrupt_sim_continued(_: Any) -> None:
     make_failure_file()
