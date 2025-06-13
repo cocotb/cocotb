7ed873b07e2a4e0b11e32575d140b083ceb2058c
Kaleb Barrett <dev.ktbarrett@gmail.com>
2025-06-12 22:57:12 -0600

    Add tests for arrays of packed structs

--- tests/designs/multi_dimension_array/cocotb_array_pkg.sv
+++ tests/designs/multi_dimension_array/cocotb_array_pkg.sv
@@ -3,4 +3,12 @@ package cocotb_array_pkg;
     typedef logic               [2:0] test_array_entry_t;
     typedef test_array_entry_t  [2:0] test_2d_array_t;
     typedef test_2d_array_t     [2:0] test_3d_array_t;
+
+    typedef struct packed {
+        logic [2:0]                 vect_packed;
+        logic [2:0][2:0]            vect_packed_packed;
+        test_array_entry_t          array_packed;
+        test_array_entry_t [2:0]    array_packed_packed;
+    } struct_packed_t;
+
 endpackage
