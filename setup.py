d40f6512f79e6fcdeb0863b86e1f8097fc17a4ab
Tomasz Hemperek <tomasz.hemperek@dectris.com>
2025-07-02 13:47:22 +0200

    Use cmake to build libraries

--- setup.py
+++ setup.py
@@ -1,28 +1,16 @@
 #!/usr/bin/env python
 # Copyright cocotb contributors
-# Copyright (c) 2013 Potential Ventures Ltd
-# Copyright (c) 2013 SolarFlare Communications Inc
 # Licensed under the Revised BSD License, see LICENSE for details.
 # SPDX-License-Identifier: BSD-3-Clause
 from __future__ import annotations
 
-import logging
 import os
 import sys
-from io import StringIO
-from os import path, walk
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
-# Note: cocotb is not installed properly yet and is missing dependencies and binaries
-# We can still import other files next to setup.py, as long as they're in MANIFEST.in
-# The below line is necessary for PEP517 support
-sys.path.append(path.dirname(__file__))
-
-from cocotb_build_libs import build_ext, get_ext
-
+# Same cap as upstream master; VERSION is the template used by setuptools-git-versioning.
 version = open("VERSION").read().strip()
-
 max_python3_minor_version = 14
 if "COCOTB_IGNORE_PYTHON_REQUIRES" not in os.environ and sys.version_info >= (
     3,
@@ -34,36 +22,5 @@ if "COCOTB_IGNORE_PYTHON_REQUIRES" not in os.environ and sys.version_info >= (
         "There is no guarantee cocotb will work with untested versions of Python and no support will be provided."
     )
 
-
-def package_files(directory):
-    paths = []
-    for fpath, _, filenames in walk(directory):
-        for filename in filenames:
-            paths.append(path.join("..", "..", fpath, filename))
-    return paths
-
-
-# store log from build_libs and display at the end in verbose mode
-# see https://github.com/pypa/pip/issues/6634
-log_stream = StringIO()
-handler = logging.StreamHandler(log_stream)
-log = logging.getLogger("cocotb_build_libs")
-log.setLevel(logging.INFO)
-log.addHandler(handler)
-
-setup(
-    cmdclass={"build_ext": build_ext},
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    package_data={
-        "cocotb": (
-            package_files("src/cocotb/share/include")
-            + package_files("src/cocotb/share/def")
-            + package_files("src/cocotb/share/lib/verilator")
-        ),
-        "cocotb_tools": (package_files("src/cocotb_tools/makefiles")),
-    },
-    ext_modules=get_ext(),
-)
-
-print(log_stream.getvalue())
+# Native libraries: CMake via scikit-build-core. Metadata: pyproject.toml.
+setup()
