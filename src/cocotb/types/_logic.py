f850f1a0122d4f76c0211c227e877850e21dcaab
Kaleb Barrett <dev.ktbarrett@gmail.com>
2025-07-22 14:22:05 -0600

    Remove DeprecationWarnings from __len__ on scalars
    These are good for both backwards compat and also writing higher-order
    functions on handles and handle values that come from either scalars or
    packed arrays. It will make people happy without making us miserable.

--- src/cocotb/types/_logic.py
+++ src/cocotb/types/_logic.py
@@ -9,7 +9,6 @@ from typing import (
     Union,
 )
 
-from cocotb._deprecation import deprecated
 from cocotb._py_compat import TypeAlias
 from cocotb.types._resolve import RESOLVE_X, ResolverLiteral, get_str_resolver
 
@@ -291,6 +290,5 @@ class Logic:
         """
         return Logic(get_str_resolver(resolver)(str(self)))
 
-    @deprecated('`len(logic)` is deprecated. A Logic\'s "length" is always 1.')
     def __len__(self) -> int:
         return 1
