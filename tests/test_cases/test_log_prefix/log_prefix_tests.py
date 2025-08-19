3d45050192e3f4886313b1e90766bdc809633777
Kaleb Barrett <dev.ktbarrett@gmail.com>
2025-08-19 07:54:55 -0600

    Add COCOTB_LOG_PREFIX to customize the log formatter's appearance

---
+++ tests/test_cases/test_log_prefix/log_prefix_tests.py
@@ -0,0 +1,47 @@
+# Copyright cocotb contributors
+# Licensed under the Revised BSD License, see LICENSE for details.
+# SPDX-License-Identifier: BSD-3-Clause
+
+
+import contextlib
+import logging
+from typing import Generator, List
+
+import cocotb
+import cocotb.ANSI as ansi
+
+
+class LogCaptureData:
+    def __init__(self) -> None:
+        self.records: List[logging.LogRecord] = []
+        self.msgs: List[str] = []
+
+
+@contextlib.contextmanager
+def capture_logs(handler: logging.Handler) -> Generator[LogCaptureData, None, None]:
+    data = LogCaptureData()
+
+    old_emit = handler.emit
+
+    def emit(record: logging.LogRecord) -> None:
+        data.records.append(record)
+        data.msgs.append(handler.format(record))
+        old_emit(record)
+
+    handler.emit = emit  # type: ignore[method-assign]
+    try:
+        yield data
+    finally:
+        handler.emit = old_emit  # type: ignore[method-assign]
+
+
+@cocotb.test
+async def test_log_prefix(_: object) -> None:
+    logger = logging.getLogger("example")
+    logger.setLevel(logging.INFO)
+    with capture_logs(logging.getLogger().handlers[0]) as logs:
+        logger.info("Test log message")
+    assert (
+        logs.msgs[0]
+        == f"{ansi.YELLOW_FG}abc{ansi.DEFAULT_FG} INFO 0       exam Test log message{ansi.DEFAULT}"
+    )
---
+++ tests/test_cases/test_log_prefix/test_log_prefix.py
@@ -0,0 +1,45 @@
+# Copyright cocotb contributors
+# Licensed under the Revised BSD License, see LICENSE for details.
+# SPDX-License-Identifier: BSD-3-Clause
+
+import os
+from pathlib import Path
+
+import pytest
+
+from cocotb_tools.runner import get_runner
+
+LANG = os.getenv("TOPLEVEL_LANG", "verilog")
+
+
+@pytest.mark.skipif(LANG != "verilog", reason="This test only supports Verilog")
+def test_indexing_warning() -> None:
+    sim = os.getenv("SIM", "icarus")
+    runner = get_runner(sim)
+
+    pwd = Path(__file__).parent.absolute()
+
+    # select args
+    build_args = []
+    test_args = []
+    if sim == "questa":
+        build_args = ["+acc"]
+        test_args = ["-t", "ps"]
+    elif sim == "xcelium":
+        build_args = ["-v93"]
+
+    runner.build(
+        sources=[pwd / "top.sv"],
+        hdl_toplevel="top",
+        build_args=build_args,
+    )
+
+    runner.test(
+        test_module="log_prefix_tests",
+        hdl_toplevel="top",
+        hdl_toplevel_lang=LANG,
+        test_args=test_args,
+        extra_env={
+            "COCOTB_LOG_PREFIX": "{ansi.YELLOW_FG}abc{ansi.DEFAULT_FG} {record.levelname} {record.created_sim_time} {record.name[:4]:>10} "
+        },
+    )
