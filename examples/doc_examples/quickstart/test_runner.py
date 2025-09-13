d15554aaf25fb63ade18cd5ccb2803723822e6c4
Georg Magneshaugen <georgmagne@gmail.com>
2025-09-13 16:57:35 +0200

    Fix docstrings to make pylint happy.

--- examples/doc_examples/quickstart/test_runner.py
+++ examples/doc_examples/quickstart/test_runner.py
@@ -1,5 +1,7 @@
-# This file is public domain, it can be freely copied without restrictions.
-# SPDX-License-Identifier: CC0-1.0
+"""
+This file is public domain, it can be freely copied without restrictions."
+SPDX-License-Identifier: CC0-1.0
+"""
 
 # test_runner.py
 
@@ -10,6 +12,10 @@ from cocotb_tools.runner import get_runner
 
 
 def test_simple_counter():
+    """
+    Creates sources list, gets a cocotb Python Runner,
+    Builds HDL, runs cocotb testcases.
+    """
     proj_path = Path(__file__).resolve().parent
 
     sources = [proj_path / "simple_counter.sv"]
