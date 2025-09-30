af8429dfe2a3d13e1a5f22161b2be8e88f12fdcd
Tymoteusz Blazejczyk <tymoteusz.blazejczyk@tymonx.com>
2025-09-30 05:28:48 +0200

    fix: import cocotb.handler is needed because of cocotb circular dependency

--- tests/pytest/test_factory.py
+++ tests/pytest/test_factory.py
@@ -8,6 +8,7 @@ from pathlib import Path
 
 import pytest
 
+import cocotb.handle  # noqa: F401 TODO: workaround for cocotb circular dependency
 from cocotb.factory import Factory
 from cocotb.regression import RegressionManager
 
