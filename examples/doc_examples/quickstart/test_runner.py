e2c72dd9f650f368c1fec0a02e3d2fdc1be059d0
Georg <43295125+georgmagne@users.noreply.github.com>
2025-09-30 21:46:15 +0200

    Make quickstart example less "basic" (#4977)
    (cherry picked from commit 2cdad006e742fff7bface9f56ddbb1e4abe96056)

--- examples/doc_examples/quickstart/test_runner.py
+++ examples/doc_examples/quickstart/test_runner.py
@@ -1,6 +1,8 @@
 # This file is public domain, it can be freely copied without restrictions.
 # SPDX-License-Identifier: CC0-1.0
 
+"""This module contains the cocotb Python test runner used to test ``simple_counter``."""
+
 # test_runner.py
 
 import os
@@ -9,21 +11,31 @@ from pathlib import Path
 from cocotb_tools.runner import get_runner
 
 
-def test_my_design_runner():
-    sim = os.getenv("SIM", "icarus")
+def test_simple_counter():
+    """Test for the simple counter.
 
+    Creates sources list, gets a cocotb Python Runner,
+    builds HDL, runs cocotb testcases.
+    """
     proj_path = Path(__file__).resolve().parent
 
-    sources = [proj_path / "my_design.sv"]
+    sources = [proj_path / "simple_counter.sv"]
 
+    sim = os.getenv("SIM", "icarus")
     runner = get_runner(sim)
+
     runner.build(
         sources=sources,
-        hdl_toplevel="my_design",
+        hdl_toplevel="simple_counter",
+        waves=True,
     )
 
-    runner.test(hdl_toplevel="my_design", test_module="test_my_design,")
+    runner.test(
+        hdl_toplevel="simple_counter",
+        test_module="simple_counter_testcases,",
+        waves=True,
+    )
 
 
 if __name__ == "__main__":
-    test_my_design_runner()
+    test_simple_counter()
