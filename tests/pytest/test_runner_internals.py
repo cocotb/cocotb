b188c2c82974ea8a28657b14ab236efc0337275b
Kaleb Barrett <dev.ktbarrett@gmail.com>
2025-10-03 11:08:41 -0600

    Apply suggestions from code review

--- tests/pytest/test_runner_internals.py
+++ tests/pytest/test_runner_internals.py
@@ -4,10 +4,13 @@ import pytest
 
 from cocotb_tools.runner import get_runner
 
+pytestmark = pytest.mark.simulator_required
+
 sim = os.getenv(
     "SIM",
     "icarus" if os.getenv("HDL_TOPLEVEL_LANG", "verilog") == "verilog" else "nvc",
 )
+
 
 @pytest.mark.parametrize("val, expected", [("1", True), (None, False), ("0", False)])
 def test_runner_booleans(val, expected):
