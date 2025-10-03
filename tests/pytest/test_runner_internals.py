4d32fd229dc460d5e55ddec7582c7da88b7c224a
Kaleb Barrett <dev.ktbarrett@gmail.com>
2025-10-03 09:33:13 -0600

    Apply suggestions from code review

--- tests/pytest/test_runner_internals.py
+++ tests/pytest/test_runner_internals.py
@@ -4,14 +4,18 @@ import pytest
 
 from cocotb_tools.runner import get_runner
 
+sim = os.getenv(
+    "SIM",
+    "icarus" if os.getenv("HDL_TOPLEVEL_LANG", "verilog") == "verilog" else "nvc",
+)
 
-@pytest.mark.parametrize("val, expected", [("1", True), ("", False), ("0", False)])
+@pytest.mark.parametrize("val, expected", [("1", True), (None, False), ("0", False)])
 def test_runner_booleans(val, expected):
-    runner = get_runner("riviera")
+    runner = get_runner(sim)
     os.environ["EXISTING"] = val
     assert runner._get_env_var_as_bool("EXISTING", False) is expected
 
 
 def test_runner_env_var_undefined():
-    runner = get_runner("riviera")
+    runner = get_runner(sim)
     assert runner._get_env_var_as_bool("NOT_EXISTING", True) is True
