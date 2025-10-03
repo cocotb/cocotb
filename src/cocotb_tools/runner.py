0462a469e6c2c475a28ff75b70c75019a8ab0b25
Kaleb Barrett <dev.ktbarrett@gmail.com>
2025-10-03 09:00:38 -0600

    Apply suggestions from code review

--- src/cocotb_tools/runner.py
+++ src/cocotb_tools/runner.py
@@ -198,12 +198,6 @@ class Runner(ABC):
         self.env["COCOTB_TEST_MODULES"] = self.test_module
         self.env["TOPLEVEL_LANG"] = self.hdl_toplevel_lang
 
-    def _get_env_var_as_bool(self, name, default) -> bool:
-        if name not in os.environ:
-            return default
-
-        return os.environ.get(name) == "1"
-
     @abstractmethod
     def _build_command(self) -> Sequence[_Command]:
         """Return command to build the HDL sources."""
@@ -455,8 +449,8 @@ class Runner(ABC):
             self.env["COCOTB_RANDOM_SEED"] = str(seed)
 
         self.log_file = log_file
-        self.waves = self._get_env_var_as_bool("WAVES", waves)
-        self.gui = self._get_env_var_as_bool("GUI", gui)
+        self.waves = bool(int(os.getenv("WAVES", waves)))
+        self.gui = bool(int(os.getenv("GUI", gui)))
         self.timescale = timescale
 
         if verbose is not None:
