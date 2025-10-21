88269d63224e71386b121776a0bec3b8005e647e
Gilberto Abram <gilberto@hudson-trading.com>
2025-10-21 19:46:37 -0400

    Handle multiple coverage files in the same directory

--- tests/test_cases/test_log_prefix/test_log_prefix.py
+++ tests/test_cases/test_log_prefix/test_log_prefix.py
@@ -19,6 +19,7 @@ def test_log_prefix() -> None:
     runner = get_runner(sim)
 
     pwd = Path(__file__).parent.absolute()
+    coverage_file = pwd / ".coverage.cocotb"
 
     # select args
     build_args = []
@@ -41,11 +42,14 @@ def test_log_prefix() -> None:
         hdl_toplevel_lang=LANG,
         test_args=test_args,
         test_filter="test_log_prefix_custom",
+        test_dir=pwd,
         extra_env={
             "COCOTB_LOG_PREFIX": "{ANSI.YELLOW_FG}abc{ANSI.DEFAULT_FG} {record.levelname} {record.created_sim_time} {record.name[:4]:>10} ",
             "COCOTB_ANSI_OUTPUT": "1",
         },
     )
+    if coverage_file.exists():
+        coverage_file.replace(pwd / ".coverage.cocotb.custom_prefix")
 
     runner.test(
         test_module="log_prefix_tests",
@@ -53,7 +57,10 @@ def test_log_prefix() -> None:
         hdl_toplevel_lang=LANG,
         test_args=test_args,
         test_filter="test_log_prefix_default",
+        test_dir=pwd,
     )
+    if coverage_file.exists():
+        coverage_file.replace(pwd / ".coverage.cocotb.reduced_prefix")
 
     runner.test(
         test_module="log_prefix_tests",
@@ -61,5 +68,8 @@ def test_log_prefix() -> None:
         hdl_toplevel_lang=LANG,
         test_args=test_args,
         test_filter="test_log_prefix_default",
+        test_dir=pwd,
         extra_env={"COCOTB_REDUCED_LOG_FMT": "0"},
     )
+    if coverage_file.exists():
+        coverage_file.replace(pwd / ".coverage.cocotb.full_prefix")
