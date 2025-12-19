fd88ceed2e1b838df09b4f5d9b438fe60f10db6a
Kaleb Barrett <dev.ktbarrett@gmail.com>
2025-12-19 15:29:37 -0700

    Add tests

---
+++ tests/test_cases/test_integers/integers.sv
@@ -0,0 +1,11 @@
+// Copyright cocotb contributors
+// Licensed under the Revised BSD License, see LICENSE for details.
+// SPDX-License-Identifier: BSD-3-Clause
+
+module top;
+byte byte_;
+shortint shortint_;
+int int_;
+longint longint_;
+integer integer_;
+endmodule
---
+++ tests/test_cases/test_integers/integers.vhdl
@@ -0,0 +1,15 @@
+-- Copyright cocotb contributors
+-- Licensed under the Revised BSD License, see LICENSE for details.
+-- SPDX-License-Identifier: BSD-3-Clause
+
+entity top is
+port (
+    integer_sig: in integer;
+    natural_sig: in natural;
+    positive_sig: in positive
+);
+end entity top;
+
+architecture rtl of top is
+begin
+end architecture;
