78988357784d4771caa0edf0a9264a564a415912
ZiaCheema <cheemazia863@gmail.com>
2026-01-07 12:40:27 +0500

    Modified file src/cocotb_tools/runner.py
    Runner.test() argument testcase to
    remove deprecated use of COCOTB_TESTCASE
    and rather update COCOTB_TEST_FILTER
    
    Fixes #5080

--- src/cocotb_tools/runner.py
+++ src/cocotb_tools/runner.py
@@ -510,9 +510,12 @@ class Runner(ABC):
 
         if testcase is not None:
             if isinstance(testcase, str):
-                self.env["COCOTB_TESTCASE"] = testcase
+                names = [s.strip() for s in testcase.split(",") if s.strip()]
             else:
-                self.env["COCOTB_TESTCASE"] = ",".join(testcase)
+                names = list(testcase)
+            escaped = [re.escape(name) for name in names]
+            regex = "^(" + "|".join(rf".*{name}" for name in escaped) + ")$"
+            self.env["COCOTB_TEST_FILTER"] = regex
 
         if test_filter is not None:
             self.env["COCOTB_TEST_FILTER"] = test_filter
