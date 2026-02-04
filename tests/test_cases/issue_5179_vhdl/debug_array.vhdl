733a03323ca1f212518c3348a4064201b8697009
ZiaCheema <ziaurrehman1145@gmail.com>
2026-02-04 16:15:02 +0500

    add vhdl design from issue #5179

--- tests/test_cases/issue_5179_vhdl/debug_array.vhdl
+++ tests/test_cases/issue_5179_vhdl/debug_array.vhdl
@@ -1,13 +1,17 @@
+--! Using 'ieee' libraries
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity debug_array is
 end entity;
 
 architecture rtl of debug_array is
+
     signal test_a : std_logic_vector(3 downto 0);
 
     type std_logic_array is array(natural range <>) of std_logic;
     signal test_b : std_logic_array(3 downto 0);
+
 begin
 end architecture;
