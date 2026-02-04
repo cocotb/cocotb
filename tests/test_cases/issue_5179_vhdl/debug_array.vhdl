bcfb2fbdb485f2fc640669f316cada09a7ea9971
ZiaCheema <ziaurrehman1145@gmail.com>
2026-02-05 01:49:00 +0500

    fix vhdl for xcelium

--- tests/test_cases/issue_5179_vhdl/debug_array.vhdl
+++ tests/test_cases/issue_5179_vhdl/debug_array.vhdl
@@ -5,17 +5,16 @@
 --! Using 'ieee' libraries
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
 
 entity debug_array is
-end entity debug_array;
+    port (
+        test_a : out std_logic_vector(3 downto 0);
+        test_b : out std_logic_vector(3 downto 0)
+    );
+end debug_array;
 
 architecture rtl of debug_array is
-
-    signal test_a : std_logic_vector(3 downto 0);
-
-    type std_logic_array is array(natural range <>) of std_logic;
-    signal test_b : std_logic_array(3 downto 0);
-
 begin
-end architecture rtl;
+    test_a <= (others => '0');
+    test_b <= (others => '0');
+end rtl;
