88bb5f0ba6a9754429093574bb316a3e893b69d0
ZiaCheema <ziaurrehman1145@gmail.com>
2026-02-04 15:20:37 +0500

    separate verilog and vhdl testing

---
+++ tests/test_cases/issue_5179_verilog/test.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+import logging
+
+import cocotb
+
+
+@cocotb.test()
+async def test_debug_array_verilog(dut):
+    tlog = logging.getLogger("cocotb.test")
+
+    def inspect_signal(signal, signal_name="name"):
+        tlog.critical(f"Signal name: {signal_name} {type(signal)}")
+
+    # TODO
+    # un-comment after PR #5272 is merged
+    # inspect_signal(dut.test_a)
+    # assert type(dut.test_a) is PackedObject
+    # inspect_signal(dut.test_b)
+    # assert type(dut.test_a) is PackedObject
+
+    try:
+        dut.test_a[0]
+    except TypeError:
+        tlog.info("Packed Object indexing failed as expected")
+        pass
+    else:
+        raise AssertionError("Verilog packed vector should not be indexable")
---
+++ tests/test_cases/issue_5179_vhdl/test.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+import logging
+
+import cocotb
+from cocotb.handle import LogicArrayObject
+
+
+@cocotb.test()
+async def test_debug_array_vhdl(dut):
+    tlog = logging.getLogger("cocotb.test")
+
+    def inspect_signal(signal, signal_name="name"):
+        tlog.info(f"Signal name: {signal_name} {type(signal)}")
+
+    inspect_signal(dut.test_a)
+    assert type(dut.test_a) is LogicArrayObject
+    inspect_signal(dut.test_b)
+    assert type(dut.test_a) is LogicArrayObject
+    # TODO
+    # un-comment after PR #5272 is merged
+    # inspect_signal(dut.test_a[0], "test_a[0]")
+    # assert type(dut.test_a[0]) is LogicObject
+    # await RisingEdge(dut.test_a[0])
