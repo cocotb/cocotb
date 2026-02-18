5d6eaf006b513fee233b2a313f5c59438162b58c
Colin Marquardt <cmarqu42@gmail.com>
2026-02-18 22:15:58 +0100

    docs: Remove version badge style from GPI page

--- docs/source/_static/cocotb.js
+++ docs/source/_static/cocotb.js
@@ -2,4 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (window.location.href.includes("release_notes")) {
         document.body.classList.add("release-notes");
     }
+    if (window.location.href.includes("library_reference_c.html")) {
+        document.body.classList.add("no-version-badges");
+    }
 });
